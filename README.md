3765126b71dd836ad02d17922ecde80cdb64784e
natj <nattila.joonas@gmail.com>
2017-07-27 17:01:18 +0300

    visualization for vlasov code

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ## Structure
 - `notes`: random (latex) notes 
 - `pic`: c++ particle-in-cell code 
-- `projects`: different projects files and tets
+- `projects`: projects files and tests
 - `prototypes`: different prototypes and test versions
 - `tools`: unix etc. tools
--- prototypes/vlasov/vlasov.py
+++ prototypes/vlasov/vlasov.py
@@ -46,6 +46,100 @@ ex, fex = efield(ex, ajx, prm)
 #3rd possible case ???
 #fex = efield_f(fex, ajx, prm)
 
+#-------------------------------------------------- 
+# visualization
+from pylab import *
+from matplotlib import cm
+import os, sys
+
+
+
+
+def plot_phasespace(ax, xxi, vxi, ffi, kk):
+
+    #pick kth species
+    xx = xxi
+    vx = vxi[:,kk]
+    ff = ffi[:,:,kk]
+
+    # xx vx[:, kk], ff[:,:,kk] ff[vx:, x:, species]
+    ax.set_xlim(xx[0], xx[-1])
+    ax.set_ylim(vx[0], vx[-1])
+    ax.set_xlabel(r'$x$')
+    ax.set_ylabel(r'$v_{x}$')
+    
+    X, Y = np.meshgrid(xx, vx)
+    ax.pcolor(X, Y, ff, 
+            cmap='Reds', 
+            vmin=ff.min(),
+            vmax=ff.max(),
+            )
+
+def plot_mean_velocity_pdf(ax, vx, ff, kk):
+
+    ax.set_xlim(-6, 6)
+    #ax.set_ylim(
+
+    ax.set_xlabel(r'$v_{x}$')
+    #ax.set_ylabel(r'pdf')
+
+    fv = np.mean(ff[:,3:prm.nx+3, kk], 1)
+    ax.plot(vx, fv, "k-")
+
+
+def plot_field(ax, xx, f, quantity):
+    ax.set_xlim(xx[0], xx[-1])
+    #ax.set_ylim(vx[0], vx[-1])
+    ax.set_xlabel(r'$x$')
+    ax.set_ylabel(quantity)
+
+    ax.plot(xx, f, "b-")
+
+
+def visualize(step):
+
+    path = "out"
+    if not os.path.exists(path):
+        os.makedirs(path)
+    
+    fname = path+'/vlasov'+str(step)+'.png'
+    
+    #set up figure
+    fig = figure(figsize=(10, 12))
+    rc('font', family='serif')
+    rc('xtick', labelsize='xx-small')
+    rc('ytick', labelsize='xx-small')
+    
+    gs = GridSpec(3, 3)
+    gs.update(hspace = 0.2)
+    gs.update(wspace = 0.2)
+    
+    ax1a = subplot(gs[0, 0:2])
+    ax1b = subplot(gs[0, 2])
+    
+    ax2a = subplot(gs[1, 0:2])
+    ax2b = subplot(gs[1, 2])
+    
+    ax3a = subplot(gs[2, 0])
+    ax3b = subplot(gs[2, 1])
+    ax3c = subplot(gs[2, 2])
+
+    #phase spaces
+    plot_phasespace(ax1a, xx, vx, ff, 0)
+    plot_mean_velocity_pdf(ax1b, vx, ff, 0)
+    
+    plot_phasespace(ax2a, xx, vx, ff, 1)
+    plot_mean_velocity_pdf(ax2b, vx, ff, 1)
+    
+    #plot fields
+    plot_field(ax3a, xx, fex, "fex")
+    plot_field(ax3b, xx, rho, "rho")
+    plot_field(ax3c, xx, ajx, "ajx")
+    
+    savefig(fname)
+
+
+
 
 #-------------------------------------------------- 
 # main loop
@@ -53,6 +147,7 @@ ex, fex = efield(ex, ajx, prm)
 jtime = 0
 for jtime in range(prm.ntime):
     print "-----------", jtime, "----------"
+    visualize(jtime)
 
     ff  = velocity_linear(ff, fex, prm)
     ff  = position_linear(ff, vx, prm)
