57c76a4dc147d792715e38d4865eb410edfc20a3
natj <nattila.joonas@gmail.com>
2017-09-03 13:00:37 +0200

    oop interface

--- prototypes/cpp2py/ex.py
+++ prototypes/cpp2py/ex.py
@@ -4,3 +4,10 @@ import example
 print example.add(1,2)
 
 
+
+p = example.Pet('Molly')
+
+print p
+print p.getName()
+p.setName('Charly')
+print p.getName()
--- prototypes/cpp2py/example.cpp
+++ prototypes/cpp2py/example.cpp
@@ -1,18 +1,33 @@
 #include <pybind11/pybind11.h>
+namespace py = pybind11;
+
+
 
 int add(int i, int j) {
     return i + j;
 }
 
+
+struct Pet {
+    Pet(const std::string &name) : name(name) { }
+    void setName(const std::string &name_) { name = name_; }
+    const std::string &getName() const { return name; }
+
+    std::string name;
+};
+
+
+
+
+// --------------------------------------------------
 PYBIND11_MODULE(example, m) {
-    m.doc() = "pybind11 example plugin"; // optional module docstring
-
     m.def("add", &add, "A function which adds two numbers");
+
+    py::class_<Pet>(m, "Pet")
+        .def(py::init<const std::string &>())
+        .def("setName", &Pet::setName)
+        .def("getName", &Pet::getName);
 }
-
-
-
-
 
 
 
