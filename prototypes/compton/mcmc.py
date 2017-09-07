494f44ba35446e1e3d1f56168e9f66585d5e6bd0
natj <nattila.joonas@gmail.com>
2017-09-07 14:08:35 +0300

    fix angle bug

--- prototypes/compton/mcmc.py
+++ prototypes/compton/mcmc.py
@@ -9,7 +9,7 @@ from matplotlib import cm
 import mcmc
 
 #set seed so we get predictable results
-np.random.seed( 1234 )
+np.random.seed( 12 )
 from mpl_toolkits.mplot3d import Axes3D
 
 
@@ -34,7 +34,7 @@ def randLoc(params):
 def randUnitSphere():
     vphi = randab( 0.0, 2.0*np.pi ) #azimuth
     vthe = randab(-np.pi/2.0, np.pi/2.0 ) #latitude
-    return (vthe, vphi)
+    return (vphi, vthe)
 
 def sph2cart(vr, vphi, vthe):
     vx = vr * np.sin(vthe) * np.cos(vphi)
@@ -42,10 +42,15 @@ def sph2cart(vr, vphi, vthe):
     vz = vr * np.cos(vthe)
     return vx, vy, vz
 
+def pol2cart(vr, vphi, vthe):
+    vx = vr * np.cos(vphi)
+    vy = vr * np.sin(vphi)
+    return vx, vy, 0.0
 
 def randVel(vabs):
     (vphi, vthe) = randUnitSphere()
     vx, vy, vz = sph2cart(vabs, vphi, vthe)
+    #vx, vy, vz = pol2cart(vabs, vphi, vthe)
     return vx, vy, vz
 
 
@@ -57,6 +62,18 @@ def unitVecZ(vec):
     return np.array([ 0.0, vec[2] ])
 
 
+def norm(vec):
+    return np.sqrt( vec[0]**2 + vec[1]**2 + vec[2]**2 )
+
+#unit vector into direction a x b
+def uCross(vecA, vecB):
+    vecC = np.cross(vecA, vecB)
+    return vecC / norm(vecC)
+
+def printVec(vec, name):
+    print " {}: ({}, {}, {}) |v| = {} ".format(name, vec[0], vec[1], vec[2], norm(vec))
+
+
 ########################################
 
 # Monte Carlo Compton scattering according to Sobol 1977
@@ -64,47 +81,63 @@ def comptonScatter(e, p, ax, plot=True):
 
     # notes about units
     # p.hv = hv/m_e c^2
-    # e.v = v/c = beta
+    # e.v = v/c 
 
     beta = np.array([ e.vx(), e.vy(), e.vz() ]) / e.v() #electron unit vector
     omeg = np.array([ p.vx(), p.vy(), p.vz() ])         #photon unit vector
     theta = np.arccos( np.dot(beta, omeg) ) #angle between electron and photon
 
+
+    #printVec(beta, "beta")
+    #printVec(omeg, "omeg")
+    #print "thet:", theta
+
+
     #rotate to scattering plane (i,j,k)
-    kvec = beta
-    jvec = np.cross(beta, omeg)
-    ivec = np.cross(kvec, jvec)
+    kvec = np.array([ 0.0, -1.0, 0.0] ) #this points along r, here we select it arbitrarily
+    #kvec = omeg #another choice so we are in direction of photon
+    jvec = uCross(beta, omeg)
+    ivec = uCross(kvec, jvec)
     M = np.array([ ivec, jvec, kvec ])
+
+    #printVec(ivec, "i")
+    #printVec(jvec, "j")
+    #printVec(kvec, "k")
+
 
     ################################################## 
     #print M
     #print np.linalg.inv(M)
 
-    #MAX = 3
-    #for direction in (-1, 1):
-    #    for point in np.diag(direction * MAX * np.array([1,1,1])):
-    #        ax.plot([point[0]], [point[1]], [point[2]], 'w')
-    #ax.plot( unitVecX(beta), unitVecY(beta), unitVecZ(beta), label='beta' )
-    #ax.plot( unitVecX(omeg), unitVecY(omeg), unitVecZ(beta), label='omeg' )
-    #ax.plot( unitVecX(ivec), unitVecY(ivec), unitVecZ(ivec), label='i' )
-    #ax.plot( unitVecX(jvec), unitVecY(jvec), unitVecZ(jvec), label='j' )
-    #ax.plot( unitVecX(kvec), unitVecY(kvec), unitVecZ(kvec), label='k' )
 
     ################################################## 
 
-    #unit vector of electron in scattering plane
-    v0  = np.matmul(M, beta)
+    #unit vector of electron in scattering coordinates
+    v0  = np.matmul(M, np.array([ e.vx(), e.vy(), e.vz()] )) #rotate electron velocity to scattering plane (i,k)
     mu  = v0[0]*np.sin(theta) +  v0[2]*np.cos(theta)
     rho = np.sqrt( v0[0]**2 + v0[1]**2 )
+
+    #printVec(v0, "v0")
     
+    #Compton parameters
     x = 2.0 * p.hv() * e.gamma() * (1.0 - mu*e.v() )
     y = x/2.0
 
-    #additional scattering angles
-    w0 = np.array([ v0[1], -v0[0], 0])/rho
-    t0 = np.array([ v0[1]*v0[2], v0[1]*v0[2], -rho**2])/rho
+    #print "Compton x: {}".format(x)
 
-    # draw scatter
+    #additional scattering angles (v0, w0, t0) define a frame of reference
+    w0 = np.array([ v0[1], -v0[0], 0])/rho
+    t0 = np.array([ v0[0]*v0[2], v0[1]*v0[2], -rho**2])/rho
+
+    #printVec(w0, "w0")
+    #printVec(t0, "t0")
+
+    #ax.plot( unitVecX(v0), unitVecY(v0), unitVecZ(v0), label='v0' )
+    #ax.plot( unitVecX(w0), unitVecY(w0), unitVecZ(w0), label='w0' )
+    #ax.plot( unitVecX(t0), unitVecY(t0), unitVecZ(t0), label='t0' )
+
+
+    #scatter
     done = False
     OmegaOmegap = 0.0
     while not(done):
@@ -128,9 +161,7 @@ def comptonScatter(e, p, ax, plot=True):
 
     #new energy
     hvp = yp / ( e.gamma()*(1.0 - mup*e.v()) ) 
-
     #print " energy shift: {}".format( hvp/p.hv() )
-
 
     #new direction
     Omegap_ijk = mup*v0 + np.sqrt(1.0-mup**2)*( w0*np.cos(phip) + t0*np.sin(phip) )
@@ -138,9 +169,10 @@ def comptonScatter(e, p, ax, plot=True):
     Omegap = Omegap / np.sqrt( Omegap[0]**2 + Omegap[1]**2 + Omegap[2]**2 ) #normalize
 
     if plot:
-        ax.plot( unitVecX(Omegap), unitVecY(Omegap), unitVecZ(Omegap) )#, label='Omegap' )
+        ax.plot( unitVecX(Omegap), unitVecY(Omegap), unitVecZ(Omegap), alpha=0.2, color='red')#, label='Omegap' )
 
     phs = mcmc.photon( hvp, Omegap[0], Omegap[1], Omegap[2] )
+
 
     return e, phs
 
@@ -167,6 +199,14 @@ if __name__ == "__main__":
     axs.append( plt.subplot(gs[1], projection='3d') )
     
 
+    ################################################## 
+    # Normalize 3d plot to make aspect ratio unity
+    MAX = 1.0
+    for direction in (-1, 1):
+        for point in np.diag(direction * MAX * np.array([1,1,1])):
+            axs[1].plot([point[0]], [point[1]], [point[2]], 'w')
+
+
 
     ################################################## 
     # set-up grid
@@ -177,13 +217,41 @@ if __name__ == "__main__":
     params.lens = [ 1.0, 1.0, 1.0 ]
 
 
+    ################################################### 
+    #test scattering for single case
+    print "Compton scatter single interaction..."
+
+    (vx, vy, vz) = randVel(0.5) 
+    e = mcmc.electron(1.0, vx, vy, vz)
+    print "target electron with beta: {} gamma: {}".format(e.v(), e.gamma() )
+
+    (vx, vy, vz) = randVel(1.0) #direction on unit sphere
+    ph = mcmc.photon( 1.0, vx, vy, vz )
+
+
+    #visualize starting point of collision
+    beta = np.array([ e.vx(), e.vy(), e.vz() ]) / e.v() #electron unit vector
+    omeg = np.array([ ph.vx(), ph.vy(), ph.vz() ])         #photon unit vector
+    axs[1].plot( unitVecX(-beta), unitVecY(-beta), unitVecZ(-beta), linestyle='dashed', color='black', linewidth=3.0)
+    axs[1].plot( unitVecX(-omeg), unitVecY(-omeg), unitVecZ(-omeg), linestyle='dotted', color='black', linewidth=4.0)
+
+
+    #es, ps = comptonScatter(e, ph, axs[1], plot=True)
+    for sc in range(100):
+        es, ps = comptonScatter(e, ph, axs[1], plot=True)
+
+
+    #plt.savefig("comptonFan.png")
+    #plt.show()
+
     ################################################## 
+    # now Monte Carlo scatter the whole bucket
     bucket = mcmc.photonBucket()
     print "created bucket ({})".format( bucket.size() )
 
     #pour isotropic photons to the bucket
-    for i in range(1000):
-        (vx, vy, vz) = randVel(0.8) #direction on unit sphere
+    for i in range(10000):
+        (vx, vy, vz) = randVel(1.0) #direction on unit sphere
         E = 1.0 # E = h\nu 
 
         ph = mcmc.photon( E, vx, vy, vz )
@@ -191,39 +259,24 @@ if __name__ == "__main__":
     print "loaded bucket with {} photons".format( bucket.size() )
 
 
-    #isotropic electron with beta = 0.5
-    (vx, vy, vz) = randVel(0.5) 
-    e = mcmc.electron(1.0, vx, vy, vz)
-    print "target electron with beta: {} gamma: {}".format(e.v(), e.gamma() )
+    xmin = 0.5
+    xmax = 100.0
+    axs[0].set_xscale('log')
+    axs[0].set_yscale('log')
 
-    
+    axs[0].set_xlim(xmin, xmax)
+    axs[0].set_ylim(0.2, 1.1)
+    axs[0].minorticks_on()
 
-    print "Compton scatter single interaction..."
-    (vx, vy, vz) = randVel(1.0) #direction on unit sphere
-    ph = mcmc.photon( 1.0, vx, vy, vz )
-
-    for sc in range(100):
-        es, ps = comptonScatter(e, ph, axs[1], plot=True)
-
-    #plt.show()
-    #axs[1].legend()
-
-    ################################################## 
-    # now Monte Carlo scatter whole bucket
-
-    #axs[0].set_xscale('log')
-    #axs[0].set_yscale('log')
-
-    axs[0].set_xlim(0.01, 12.0)
-    axs[0].set_ylim(0.1, 1.1)
 
     #step in time
     xs = np.zeros( bucket.size() )
-    for lap in range(5):
+    for lap in range(7):
+        print("lap : {}").format(lap)
         for i in range(bucket.size() ):
             
             #draw random electron
-            (vx, vy, vz) = randVel( 0.9  ) 
+            (vx, vy, vz) = randVel( 0.9 ) 
             e = mcmc.electron(1.0, vx, vy, vz)
 
             p = bucket.get(i)
@@ -234,9 +287,10 @@ if __name__ == "__main__":
             bucket.replace(i, ps)
 
             
-    hist, edges = np.histogram(xs, np.linspace(1.1, 2.0, 50))
-    hist = hist / np.max(hist)
-    axs[0].plot(edges[:-1], hist, "k-")
+        #hist, edges = np.histogram(xs, np.linspace(xmin, xmax, 20))
+        hist, edges = np.histogram(xs, np.logspace(np.log10(xmin), np.log10(xmax), 20))
+        hist = 1.0 * hist / hist.max()
+        axs[0].plot(edges[:-1], hist )#, "k-")
 
 
     #visualize_data(axs[0], mesh, params)
