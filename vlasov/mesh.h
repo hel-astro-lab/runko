c0a6fca3d388957e11ecb0a4eac8f956b99cf0cb
natj <nattila.joonas@gmail.com>
2018-01-08 12:05:18 +0100

    WIP: introduce adaptive mesh interface

--- vlasov/mesh.h
+++ vlasov/mesh.h
@@ -22,15 +22,16 @@ class Mesh {
   typedef std::array<T, D> value_t;
 
 
-  static const uint64_t error_block = 0;
+  static const uint64_t error_cid = 0;
   static const uint64_t error_index = 0xFFFFFFFFFFFFFFFF;
-  int max_refinement_level;
+  int max_refinement_level = 10;
 	uint64_t last_cell;
   size_t number_of_blocks = 0;
   
   std::unordered_map<uint64_t, T> data;
 
 
+  /*
   template<>
   uint64_t index(const std::array<uint64_t, 1>& indices) const {}
 
@@ -41,20 +42,117 @@ class Mesh {
   uint64_t index(const std::array<uint64_t, 3>& indices) const {
 		uint64_t cid = 1;
 
-    return cid
+    return cid;
   }
 
   uint64_t index(const std::array<uint64_t, D>& indices) const {}
-
+  */
 
 
   
-  indices_t get_indices(uint64_t cid) const {}
+  indices_t get_indices(uint64_t cid) const 
+  {
+
+    if (cid == error_cid || cid > this->last_cid) {
+      const indices_t error_indices = {{error_index, error_index, error_index}};
+      return error_indices;
+    }
+
+    // subtract ids of larger cells
+    const int refinement_level = this->get_refinement_level(cid);
+    for (int i = 0; i < refinement_level; i++) {
+      cid -=
+        this->length.get()[0]
+        * this->length.get()[1]
+        * this->length.get()[2]
+        * (uint64_t(1) << (i * 3));
+    }
+
+    cid -= 1;	// cell numbering starts from 1
+    const indices_t indices = {{
+
+      (cid % (this->length.get()[0] * (uint64_t(1) << refinement_level)))
+        * (uint64_t(1) << (max_refinement_level - refinement_level)),
+
+        ((cid / (this->length.get()[0] * (uint64_t(1) << refinement_level)))
+         % (this->length.get()[1] * (uint64_t(1) << refinement_level)))
+          * (uint64_t(1) << (max_refinement_level - refinement_level)),
+
+        (cid / (
+                 this->length.get()[0]
+                 * this->length.get()[1]
+                 * (uint64_t(1) << (2 * refinement_level))
+                ))
+          * (uint64_t(1) << (max_refinement_level - refinement_level))
+    }};
+
+    return indices;
+
+
+  }
+
 
   uint64_t get_cell(
       const indices_t& indices,
       const int refinement_level
-  ) const { }
+  ) const 
+  { 
+
+    if (indices[0] >= this->length.get()[0] * (uint64_t(1) << this->max_refinement_level)) {
+      return error_cid;
+    }
+
+    if (indices[1] >= this->length.get()[1] * (uint64_t(1) << this->max_refinement_level)) {
+      return error_cid;
+    }
+
+    if (indices[2] >= this->length.get()[2] * (uint64_t(1) << this->max_refinement_level)) {
+      return error_cid;
+    }
+
+    if (refinement_level < 0) {
+      return error_cid;
+    }
+
+    if (refinement_level > this->max_refinement_level) {
+      return error_cid;
+    }
+
+    // cell numbering starts at 1
+    uint64_t cid = 1;
+
+    // add ids of larger cells
+    for (int i = 0; i < refinement_level; i++) {
+      cid +=
+        this->length.get()[0]
+        * this->length.get()[1]
+        * this->length.get()[2]
+        * (uint64_t(1) << (i * 3));
+    }
+
+    // convert to indices of this cell's refinement level
+    const indices_t this_level_indices = {{
+      indices[0] / (uint64_t(1) << (this->max_refinement_level - refinement_level)),
+        indices[1] / (uint64_t(1) << (this->max_refinement_level - refinement_level)),
+        indices[2] / (uint64_t(1) << (this->max_refinement_level - refinement_level))
+    }};
+
+    // get the length of the grid in terms of cells of this refinement level
+    const std::array<uint64_t, 2> this_level_length = {{
+      this->length.get()[0] * (uint64_t(1) << refinement_level),
+        this->length.get()[1] * (uint64_t(1) << refinement_level)
+    }};
+
+    cid
+      += this_level_indices[0]
+      + this_level_indices[1] * this_level_length[0]
+      + this_level_indices[2] * this_level_length[0] * this_level_length[1];
+
+    return cid;
+
+  
+  
+  }
     
   int get_refinement_level(const uint64_t cid) const {}
 
