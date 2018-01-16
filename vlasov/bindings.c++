73c0b7ab92b74039ad4d7b52d96c181af6e61453
natj <nattila.joonas@gmail.com>
2018-01-16 15:10:08 +0100

    adapter framework

--- vlasov/bindings.c++
+++ vlasov/bindings.c++
@@ -5,6 +5,7 @@ namespace py = pybind11;
 
 #include "mesh.h"
 #include "amr_numerics.h"
+#include "amr_refiner.h"
 
 
 
@@ -12,7 +13,9 @@ PYBIND11_MODULE(pyplasmaDev, m) {
 
   typedef float Realf;
   // typedef toolbox::AdaptiveMesh<Realf, 1> AM1d;
+    
   typedef toolbox::AdaptiveMesh<Realf, 3> AM3d;
+  typedef toolbox::Adapter<Realf, 3> Adapter3d;
 
 
 
@@ -30,20 +33,18 @@ PYBIND11_MODULE(pyplasmaDev, m) {
     .def("get_level_0_parent_indices", &AM3d::get_level_0_parent_indices)
     .def("get_level_0_parent",   &AM3d::get_level_0_parent)
     .def("get_children",         &AM3d::get_children)
+    .def("get_cells",            &AM3d::get_cells)
     .def("__getitem__", [](const AM3d &s, py::tuple indx) 
         { 
         uint64_t i = indx[0].cast<uint64_t>();
         uint64_t j = indx[1].cast<uint64_t>();
         uint64_t k = indx[2].cast<uint64_t>();
-        int   rfl = indx[3].cast<int>();
-
-        // const AM3d::indices_t cindx = {{i,j,k}};
-        // uint64_t cid = s.get_cell_from_indices(cindx, rfl);
+        int    rfl = indx[3].cast<int>();
         uint64_t cid = s.get_cell_from_indices({{i,j,k}}, rfl);
 
         if(cid == AM3d::error_cid) {throw py::index_error();}
 
-        return s.get(cid);
+        return s.get_from_roots(cid);
         })
     .def("__setitem__", [](AM3d &s, py::tuple indx, Realf v) 
         { 
@@ -51,29 +52,44 @@ PYBIND11_MODULE(pyplasmaDev, m) {
         uint64_t j = indx[1].cast<uint64_t>();
         uint64_t k = indx[2].cast<uint64_t>();
         int   rfl = indx[3].cast<int>();
-
-        // const AM3d::indices_t cindx = {{i,j,k}};
-        // uint64_t cid = s.get_cell_from_indices(cindx, rfl);
-          
         uint64_t cid = s.get_cell_from_indices({{i,j,k}}, rfl);
 
         if(cid == AM3d::error_cid) {throw py::index_error();}
 
         s.set(cid, v);
         })
-    .def("clip",       &AM3d::clip)
-    .def("set_min",    &AM3d::set_min)
-    .def("set_max",    &AM3d::set_max)
-    .def("get_length", &AM3d::get_length)
-    .def("get_center", &AM3d::get_center)
+    .def("clip_cells",       &AM3d::clip_cells)
+    //.def("cut_roots",        &AM3d::cut_roots)
+    .def("split",                   &AM3d::split)
+    .def("update_from_children",    &AM3d::update_from_children)
+    .def("update_from_leafs",       &AM3d::update_from_leafs)
+    .def("get_from_leafs",          &AM3d::get_from_leafs)
+    .def("is_leaf",                 &AM3d::is_leaf)
+    .def("set_min",                 &AM3d::set_min)
+    .def("set_max",                 &AM3d::set_max)
+    .def("get_length",              &AM3d::get_length)
+    .def("get_center",              &AM3d::get_center)
     .def("get_level_0_cell_length", &AM3d::get_level_0_cell_length);
 
 
-    m.def("deriv", &toolbox::deriv<Realf, 3>);
-    m.def("grad",  &toolbox::grad<Realf, 3>);
+
+    // -------------------------------------------------- 
+    // AMR numerics
+      
+  m.def("deriv", &toolbox::deriv<Realf, 3>);
+
+  m.def("grad",  &toolbox::grad<Realf, 3>);
 
 
 
+  py::class_<Adapter3d>(m, "Adapter")
+    .def(py::init<>())
+    .def_readwrite("tolerance", &Adapter3d::tolerance)
+    .def_readwrite("cells_to_refine",   &Adapter3d::cells_to_refine)
+    .def_readwrite("cells_to_unrefine", &Adapter3d::cells_to_unrefine)
+    .def_readwrite("cells_created",     &Adapter3d::cells_created)
+    .def("check",     &Adapter3d::check)
+    .def("refine",    &Adapter3d::refine);
 
 
 
--- vlasov/dev2.py
+++ vlasov/dev2.py
@@ -79,10 +79,76 @@ def level_fill(m, rfl):
                 m[i,j,k, rfl] =  val
 
 
+def adaptive_fill(m, tol=0.001):
+
+    nx, ny, nz = m.get_length(0)
+    for i in range(nx):
+        for j in range(ny):
+            for k in range(nz):
+                x,y,z = m.get_center([i,j,k], 0)
+                val = gauss(x,y,z)
+                m[i,j,k, 0] =  val
+
+    adapter = pdev.Adapter();
+
+
+    adapter.check(m)
+    adapter.refine(m)
+    for cid in adapter.cells_created:
+        rfl = m.get_refinement_level(cid)
+        indx = m.get_indices(cid)
+        x,y,z = m.get_center(indx, rfl)
+
+        print("new cell {} at ({},{},{})/{}".format(cid,
+            indx[0],
+            indx[1],
+            indx[2],
+            rfl))
+
+        val = gauss(x,y,z)
+        m[indx[0], indx[1], indx[2], rfl] = val
+        
+
+
+
+    #next refine grid until tolerance
+    #for sweep in [0]:
+    if False:
+
+        for cid in m.get_cells(True):
+            rfl = m.get_refinement_level(cid)
+
+            if(not m.is_leaf(cid) ):
+                continue
+
+            indx = m.get_indices(cid)
+            grad = pdev.grad(m, indx, rfl)
+
+            # our error indicator is | max{ Grad(f) } |
+            err = np.max( np.abs(grad))
+            #print("refining err: {}".format(err))
+            if (err > 0.001):
+            #if True:
+                #m.split(cid) #let's not remove the parent
+
+                #fill children with correct values
+                for cidc in m.get_children(cid):
+                    indxc    = m.get_indices(cidc)
+                    xc,yc,zc = m.get_center(indxc, rfl+1)
+
+                    val = 0.8*gauss(xc,yc,zc)
+                    m[indxc[0], indxc[1], indxc[2], rfl+1] = val
+
+                #m.update_from_children(cid)
+
+        #induced refining goes here
+
+
+
+
 
 def get_mesh(m, args):
     rfl = args["rfl"]
-
     n1, n2, n3 = m.get_length(rfl)
 
     #flip dimensions to right-handed coordinate system
@@ -158,6 +224,7 @@ def plot2DSlice(ax, m, args):
 
     mesh = get_mesh(m, args)
 
+
     #normalize
     mesh.ff = mesh.ff / np.max(mesh.ff)
 
@@ -168,9 +235,75 @@ def plot2DSlice(ax, m, args):
            vmin = 0.0,
            vmax = 1.0,
            cmap = "plasma_r",
-           clip = 0.001
+           clip = 0.0
            )
     return
+
+
+def plotAdaptiveSlice(ax, m, args):
+
+    rfl_max = args["rfl"]
+    nx, ny, nz = m.get_length(rfl_max)
+
+    #empty arrays ready
+    xx = np.zeros((nx))
+    yy = np.zeros((ny))
+    ff = np.zeros((nx, ny))
+
+    for i in range(nx):
+        x,y,z = m.get_center([i,0,0], rfl_max)
+        xx[i] = x
+    for j in range(ny):
+        x,y,z = m.get_center([0,j,0], rfl_max)
+        yy[j] = y
+
+
+    if args["q"] == "mid":
+            q = nz/2
+    else:
+        q = args["q"]
+
+
+    #adapter = pdev.Adapter();
+    #adapter.check(m)
+    #cells = adapter.cells_to_refine
+    #cells = m.get_cells(True)
+    #for cid in cells:
+    #        rfli = m.get_refinement_level(cid)
+    #        indx  = m.get_indices(cid)
+    #        i,j,k = indx
+    #        if k != q:
+    #            continue
+    #        if rfli != rfl_max:
+    #            continue
+    #        val = m[i,j,k, rfli]
+    #        ff[i,j] = val
+
+
+    for i in range(nx):
+        for j in range(ny):
+            indx = [i,j,q]
+            val = m[i,j,q,rfl_max]
+
+            ff[i,j] = val
+
+
+
+
+
+    imshow(ax,
+           ff,
+           xx[0], xx[-1],
+           yy[0], yy[-1],
+           vmin = 0.0,
+           vmax = 0.02,
+           cmap = "plasma_r",
+           clip = 0.0
+           )
+    return
+
+
+
 
 
 def get_guide_grid(m, rfl):
@@ -236,15 +369,18 @@ def plotGradientSlice(ax, m, args):
     X, Y  = np.meshgrid(xx, yy)
     U     = gg[:,:,0]
     V     = gg[:,:,1]
-    speed = np.sqrt(U*U + V*V)
+    #speed = np.sqrt(U*U + V*V)
     #speed = np.sqrt(U*U)
     #speed = np.sqrt(V*V)
+
+    speed = np.zeros((nx, ny))
+    for i in range(nx):
+        for j in range(ny):
+            speed[i,j] = np.maximum( np.abs(U[i,j]), np.abs(V[i,j]) )
+
         
-    lw = 5*speed / speed.max()
-    ax.streamplot(X, Y, U, V, density=0.9, color="k", linewidth=lw)
-
-    #ax.quiver(X, Y, U, V, units='x', pivot='tip', width=0.022) #, scale=1/0.15)
-
+    #errs = np.max( np.abs(speed) )
+ 
     imshow(ax,
            speed / speed.max(),
            xx[0], xx[-1],
@@ -255,6 +391,11 @@ def plotGradientSlice(ax, m, args):
            clip = 0.0
            )
 
+
+    lw = 5*speed / speed.max()
+    ax.streamplot(X, Y, U, V, density=0.9, color="k", linewidth=lw)
+
+    #ax.quiver(X, Y, U, V, units='x', pivot='tip', width=0.022) #, scale=1/0.15)
     
 def saveVisz(lap, conf):
     slap = str(lap).rjust(4, '0')
@@ -275,16 +416,18 @@ if __name__ == "__main__":
 
     print("max. possible refinement:", m.get_maximum_possible_refinement_level())
 
-    level_fill(m, 0)
-    level_fill(m, 1)
-    level_fill(m, 2)
+    #level_fill(m, 0)
+    #level_fill(m, 1)
+    #level_fill(m, 2)
     #level_fill(m, 3)
+
+    adaptive_fill(m)
 
 
 
     ################################################## 
     # set up plotting and figure
-    plt.fig = plt.figure(1, figsize=(8,12))
+    plt.fig = plt.figure(1, figsize=(8,20))
     plt.rc('font', family='serif', size=12)
     plt.rc('xtick')
     plt.rc('ytick')
@@ -302,6 +445,7 @@ if __name__ == "__main__":
             "q":  "mid",
             "rfl": 2 }
     #plot2DSlice(axs[0], m, args)
+    #plotAdaptiveSlice(axs[0], m, args)
     plotGradientSlice(axs[0], m, args)
 
     args = {"dir":"xz", 
@@ -316,7 +460,26 @@ if __name__ == "__main__":
     plot2DSlice(axs[2], m, args)
 
 
+
+
+
+    indx = [5,29,3]
+    print(indx) 
+    pindx1 = m.get_parent_indices(indx)
+    print(pindx1)
+    pindx2 = m.get_parent_indices(pindx1)
+    print("1:", pindx2)
+
+    print("2:",m.get_level_0_parent_indices(indx, 2))
     
+
+    print(indx) 
+    indx= m.get_parent_indices(indx)
+    print(indx)
+    indx = m.get_parent_indices(indx)
+    print("3:", indx)
+    
+    #m.cut()
 
 
 
