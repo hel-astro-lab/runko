df28da7db5f00ade36ce18d99ca6fcc5da4f4db7
natj <nattila.joonas@gmail.com>
2018-09-27 11:34:36 +0200

    update ts-analysis to read analysis/fields files

--- projects/tests/analysis-twostream.py
+++ projects/tests/analysis-twostream.py
@@ -5,43 +5,70 @@ import sys, os
 import matplotlib.ticker as ticker
 from scipy.stats import mstats
 
+from combine_files import combine_files, combine_tiles
+from configSetup import Configuration
 
+def flatten_spatial(arr):
+    return arr.reshape( arr.shape[:-3] + (-1,) ).T
+
+
+
+read_files = True
+read_runf = False
 
 #--------------------------------------------------
 # read simulation data from file
-f = h5py.File('twostream/out/run.hdf5','r')
-#f = h5py.File('twostream/out_ub2/run.hdf5','r')
-#f = h5py.File('twostream_kebne/ub1.h5','r')
-#f = h5py.File('twostream_kebne/ub4.h5','r')
-#f = h5py.File('twostream_kebne/ub4_noclip.h5','r')
-#f = h5py.File('twostream_kebne/ub4_hiresv.h5','r')
+#fdir = 'twostream/out/'
+fdir = 'twostream_kebne/ub2/'
 
-#f = h5py.File('twostream/out_limits_lowres128/run.hdf5','r')
-#f = h5py.File('bump-on-tail/out/run.hdf5','r')
 
-#maxi = 4000
+if read_files:
+    #conf = Configuration('config-twostream-relativistic.ini') 
+    conf = Configuration(fdir+'config-ts2.ini') 
+    #--------------------------------------------------
+    
+    print("files...")
+    ex_sparse =  flatten_spatial( combine_files(fdir, "field",    "ex",   conf) ) 
+    #ey_sparse =  flatten_spatial( combine_files(fdir, "field",    "ey",   conf) )
+    #ez_sparse =  flatten_spatial( combine_files(fdir, "field",    "ez",   conf) )
+    #bx_sparse =  flatten_spatial( combine_files(fdir, "field",    "bx",   conf) )
+    #by_sparse =  flatten_spatial( combine_files(fdir, "field",    "by",   conf) )
+    #bz_sparse =  flatten_spatial( combine_files(fdir, "field",    "bz",   conf) )
+    rh_sparse =  flatten_spatial( combine_files(fdir, "field",    "rho",  conf) )
+    ed_sparse1 = flatten_spatial( combine_files(fdir, "analysis", "edens",conf, isp=0) )
+    ed_sparse2 = flatten_spatial( combine_files(fdir, "analysis", "edens",conf, isp=1) )
+    ed_sparse = ed_sparse1 + ed_sparse2
 
-#Read field
-ex   = f['fields/Ex'][()]
-rho  = f['fields/rho'][()]
-ekin = f['fields/ekin'][()]
+    dt = conf.dt
+    dx = conf.dx
 
-print("Ex shape:", np.shape(ex))
+    nx, nt = np.shape(ex_sparse)
+    time_s = np.arange(nt)*dt*conf.interval
+    
+    print("nt,nx", np.shape(ex_sparse))
+    print("time_s:", time_s)
 
-#Read simulation values
-dt = f['params'].attrs['dt']
-dx = f['params'].attrs['dx']
-print(dt, dx)
 
-nx, ny = np.shape(ex)
+if read_runf:
+    f = h5py.File(fdir+'run.hdf5','r')
+    dt = f['params'].attrs['dt']
+    dx = f['params'].attrs['dx']
+    print(dt, dx)
 
-time = np.arange(ny)*dt
-#maxtime = time[maxi]
 
-#ex_max = np.zeros((ny))
-#for i in range(ny):
-#    exs     = ex[:,i]
-#    ex_max[i] = np.max(np.abs(exs))
+    #--------------------------------------------------
+    #Read field
+    ex   = f['fields/Ex'][()]
+    rho  = f['fields/rho'][()]
+    ekin = f['fields/ekin'][()]
+    
+    print("Ex shape:", np.shape(ex))
+    
+    #Read simulation values
+    nx, ny = np.shape(ex)
+    time = np.arange(ny)*dt
+
+
 
 
 #--------------------------------------------------
@@ -67,14 +94,7 @@ axs.append( plt.subplot(gs[4,0]) )
 for ax in axs:
     ax.minorticks_on()
     ax.set_xlabel(r'time $t\omega_{\mathrm{p}}$ ')
-
-    #ax.set_xlim((0.0, maxtime))
-    #ax.set_xlim((0.0, 50.0))
-    #ax.set_xlim((0.0, 70.0))
-    #ax.set_xlim((0.0, 54.0))
-    #ax.set_xlim((0.0, 400.0))
-    #ax.set_xlim((250.0, 400.0))
-    #ax.set_xlim((0.0, 1200.0))
+    ax.set_xlim((0.0, 50.0))
 
 
 axs[0].set_ylabel(r'$\ln \delta E_x$')
@@ -84,102 +104,134 @@ axs[3].set_ylabel(r'$\epsilon_K$')
 axs[4].set_ylabel(r'$E_T$')
 
 
-#time *= 0.321003 #normalize with the growth rate
-ex_max = np.max( np.abs(ex),0 )
-axs[0].plot(time, np.log(ex_max))
+if read_runf:
 
-#axs[0].set_ylim(-14.0, 1.0)
-#axs[0].set_ylim(-20.0, 1.0)
-axs[0].set_ylim(-20.0, -5.0)
+    #time *= 0.321003 #normalize with the growth rate
+    ex_max = np.max( np.abs(ex),0 )
+    axs[0].plot(time, np.log(ex_max))
+    
+    
+    
 
-
-#relativistic (vb = 1.0)
-#Gm = 0.21
-#Gms = -8.0 + time*Gm
-
-#classical (vb = 0.025)
-#Gm = 0.24377
-#Gms = -8.0 + time*Gm
-
-#Gm = 0.35
-#Gms = -18.5 + time*Gm
-
-#Gm = 0.25 (vth 0.1)
-#Gm = 0.17
-#Gms = -21.0 + time*Gm
+if read_files:
+    ex_max_s = np.max( np.abs(ex_sparse), 0)
+    axs[0].plot(time_s, np.log(ex_max_s))
 
 
-#Gm = 0.32 #(vth=0.05)
-#Gms = -18.3 + time*Gm 
-
-
-#bump-on-tail
-#Gm = 0.12
-#Gm = 0.040
-#Gms = -22.7 + time*Gm
-
+##################################################
+# growth rates
 
 #gammab = 1.414
 gammab = 2.236
 #gammab = 3.162 
 #gammab = 4.123
-
+    
 Gi = 1.0/np.sqrt(8.0*gammab**3.0)
 #Gms = -14.0 + time*Gi #ub1
-Gms = -13.5 + time*Gi #ub2
 
-axs[0].plot(time, Gms, 'r--')
+if read_runf:
+    Gms = -8.5 + time*Gi #ub2
+    axs[0].plot(time, Gms, 'r--')
 
-##################################################
-
-
-wedens = np.log10( np.sum( ex*ex, 0 ) )
-axs[1].plot(time, wedens)
-
-#model prediction
-#Gm = 0.243771 #vb = 0.025
-#Gm = 0.0 + 0.21j
-
-#Gm = 0.21
-#Gms = -3.0 + time*Gm*2.0 # 1/2 comes from compensation of E_x^2
-
-Gms = -9.0 + time*Gi # 1/2 comes from compensation of E_x^2
-axs[1].plot(time, Gms, 'r--')
+if read_files:
+    Gms = -8.5 + time_s*Gi #ub2
+    axs[0].plot(time_s, Gms, 'r--')
 
 
-#axs[1].set_ylim(-10.0, 4.0)
-#axs[1].set_ylim(-18.0, 4.0)
-
-##################################################
-
-prtcls = np.sum(rho, 0) #integrate particle density
-#prtcls /= prtcls[0]
-
-prtcls = np.abs(prtcls - prtcls[1] )/prtcls[1]
-#prtcls = np.clip(prtcls, 1.0e-8, 1.0e2)
-
-print("prtcls:", prtcls)
-axs[2].plot(time, np.log10(prtcls))
-axs[2].set_ylim((-8.0, 0.0))
-#axs[2].plot(time, prtcls)
-
-##################################################
-
-ekintot = np.sum(ekin, 0)
-axs[3].plot(time, np.log10(ekintot))
+#axs[0].set_ylim(-14.0, 1.0)
+#axs[0].set_ylim(-20.0, 1.0)
+axs[0].set_ylim(-10.0, -5.0)
 
 
 ##################################################
-print("ekin   max:", np.max(ekintot))
-print("efield max:", np.max(wedens))
-print("ratio:", np.mean(ekintot)/np.mean(wedens))
 
-ekintot = ekintot * dx
+if read_runf:
+    wedens = np.log10( np.sum( ex*ex, 0 ) )
+    axs[1].plot(time, wedens)
+    
+    #model prediction
+    #Gm = 0.243771 #vb = 0.025
+    #Gm = 0.0 + 0.21j
+    
+    #Gm = 0.21
+    #Gms = -3.0 + time*Gm*2.0 # 1/2 comes from compensation of E_x^2
+    
+    Gms = -5.2 + time*Gi # 1/2 comes from compensation of E_x^2
+    axs[1].plot(time, Gms, 'r--')
+    
+    #axs[1].set_ylim(-10.0, 4.0)
+    #axs[1].set_ylim(-18.0, 4.0)
 
-etot = ekintot + wedens
-axs[4].plot(time, np.log10( etot),    "k-" )
-axs[4].plot(time, np.log10( ekintot), "b--")
-axs[4].plot(time, np.log10( wedens),  "r--")
+
+if read_files:
+    wedens_s = np.sum( ex_sparse**2.0, 0)
+    axs[1].plot(time_s, np.log10(wedens_s))
+
+    Gms = -5.2 + time_s*Gi # 1/2 comes from compensation of E_x^2
+    axs[1].plot(time_s, Gms, 'r--')
+
+
+##################################################
+axs[2].set_yscale('log')
+
+if read_runf:
+    prtcls = np.sum(rho, 0) #integrate particle density
+    #prtcls /= prtcls[0]
+    
+    prtcls = np.abs(prtcls - prtcls[1] )/prtcls[1]
+    #prtcls = np.clip(prtcls, 1.0e-8, 1.0e2)
+    
+    print("prtcls:", prtcls)
+    axs[2].plot(time, prtcls)
+    axs[2].set_ylim((-8.0, 0.0))
+    #axs[2].plot(time, prtcls)
+
+if read_files:
+    dens = np.sum(rh_sparse, 0)
+    dens = np.abs(dens - dens[1])/dens[1] #normalize to initial value
+
+    axs[2].plot(time_s, dens)
+    axs[2].set_ylim((1.0e-7, 1.0e1))
+
+
+
+##################################################
+# units
+ef_units = dt*2.
+ef_units = 8.0*dx*(dt/dx)**2.
+print("ef_units", ef_units)
+
+
+if read_runf:
+    ekintot = np.sum(ekin, 0)
+    axs[3].plot(time, np.log10(ekintot))
+
+
+    ##################################################
+    print("ekin   max:", np.max(ekintot))
+    print("efield max:", np.max(wedens))
+    print("ratio:", np.mean(ekintot)/np.mean(wedens))
+    
+    ekin = np.sum(ekin, 0) #E_kin density
+    
+    eef = np.sum(ex*ex, 0) #E_x density
+    eef /= ef_units
+    
+    etot = ekin + eef
+    axs[4].plot(time, etot,    "k-" )
+    axs[4].plot(time, ekin, "b--")
+    axs[4].plot(time, eef,  "r--")
+
+
+if read_files:
+    # sparse sampling
+    ekin_s = np.sum(ed_sparse,0)
+    eef_s  = np.sum(ex_sparse**2.0,0)/ef_units
+    etot_s = ekin_s + eef_s
+    
+    axs[4].plot(time_s, etot_s, "k." )
+    axs[4].plot(time_s, ekin_s, "b.")
+    axs[4].plot(time_s, eef_s,  "r.")
 
 
 
