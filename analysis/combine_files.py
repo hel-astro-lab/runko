8c046b255298630a8058ab2c7ae5c913caf45ddf
natj <nattila.joonas@gmail.com>
2018-11-22 18:12:53 +0200

    create preprocess file for tiles

--- analysis/combine_files.py
+++ analysis/combine_files.py
@@ -2,6 +2,7 @@ import numpy as np
 import h5py
 import glob
 import re
+import os
 
 from configSetup import Configuration
 
@@ -38,22 +39,103 @@ def combine_files(fdir,
                   isp=None, 
                   func=None):
 
-    fstr = fdir + fname + "*.h5"
+    fstr = fdir + fname + '-*_*.h5'
     files = glob.glob(fstr) 
-
     files.sort(key=natural_keys)
-    #print(files)
 
+    #last file
+    #print(files)
+    #print(files[-1])
+
+    fname_prepro = fdir + "processed-" + fname + "-" + var + "_" + str(isp) + ".h5"
+    #print(fname_prepro)
+
+    last_file_num = atoi( re.split('(\d+)', files[-1].strip())[-4] )
+    print("newest file is: ", last_file_num)
+
+    if os.path.isfile(fname_prepro):
+        print("reading from preprocesses file: ", fname_prepro)
+        f5 = h5py.File(fname_prepro,'r')
+        dset = f5['processed']
+        newest_snapshot = dset.attrs['current_head']
+
+        print(" latest processed file is ", newest_snapshot)
+
+        #use what is in the file and read the rest, then update the tail 
+        if newest_snapshot < last_file_num:
+            #print(" adding files from {} to {}".format(newest_snapshot, last_file_num))
+
+            # skim file list so that only newest additions are left
+            # this way we do not reprocess stuff
+
+            i = 0
+            for ff in files:
+                file_num = atoi( re.split('(\d+)', ff.strip())[-4] )
+                if file_num > newest_snapshot:
+                    break
+                else:
+                    i += 1
+            #print(" using only files")
+            #print(files[i:])
+            files = files[i:]
+
+            #TODO modify list
+            f5.close()
+
+        #preprocessed file is at the tip so just read and give back
+        else:
+            #print(" tail is at the newest file")
+            data = np.array(f5['processed'][:,:,:,:])
+            f5.close()
+
+            return data
+
+
+    # actual file reading
     ret_arr = []
-    
     for ff in files:
         arr = combine_tiles(ff, var, conf, isp=isp)
         
         if not(func == None):
             arr = func(arr)
         ret_arr.append( arr )
+    data = np.array(ret_arr)
 
-    return np.array(ret_arr)
+
+    # process quick read file
+
+    #create it if it does not exist
+    if not(os.path.isfile(fname_prepro)):
+        #print(" creating preprocess file")
+        f5 = h5py.File(fname_prepro,'w')
+        dset = f5.create_dataset('processed', data=data)
+        dset.attrs['current_head'] = last_file_num
+
+        f5.close()
+
+    #else update tail of processed file
+    else:
+        if newest_snapshot < last_file_num:
+            #print("updating tail from {} to {}".format(newest_snapshot, last_file_num))
+            f5 = h5py.File(fname_prepro,'r+')
+
+            data_old = np.array(f5['processed'][:,:,:,:])
+            #print(np.shape(data_old))
+            #print(np.shape(data))
+
+            data_new = np.append(data_old, data, axis=0)
+            #print(np.shape(data_new))
+
+            #update f5 file
+            del f5['processed']
+            dset = f5.create_dataset('processed', data=data_new)
+            dset.attrs['current_head'] = last_file_num
+            f5.close()
+
+            return data_new
+
+
+    return data
 
 
 
