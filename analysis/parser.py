33200167545609e9345575cf0a78ccb6cd61963e
natj <nattila.joonas@gmail.com>
2019-01-24 15:34:14 +0100

    add parser for variable

--- analysis/parser.py
+++ analysis/parser.py
@@ -9,16 +9,32 @@ from configSetup import Configuration
 def parse_input():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--conf', dest='conf_filename', default=None,
-                       help='Name of the configuration file (default: None)')
+    parser.add_argument('-c', '--conf', 
+            dest='conf_filename', 
+            default=None,
+            type=str,
+            help='Name of the configuration file (default: None)')
 
-    parser.add_argument('-l', '--lap', dest='lap', default=None,
-                       help='Specific lap to analyze (default: all that can be found)')
+    parser.add_argument('-l', '--lap', 
+            dest='lap', 
+            default=None,
+            type=int,
+            help='Specific lap to analyze (default: all that can be found)')
 
-    parser.add_argument('-d', '--dir', dest='fdir', default=None,
-                       help='Directory to analyze')
+    parser.add_argument('-d', '--dir', 
+            dest='fdir', 
+            default=None,
+            type=str,
+            help='Directory to analyze')
+
+    parser.add_argument('-v', '--var', 
+            dest='var', 
+            default=None,
+            type=str,
+            help='Variable to analyze')
 
     args = parser.parse_args()
+    #args = parser.parse_known_args()
 
     if not(args.conf_filename == None):
         conf = Configuration(args.conf_filename)
