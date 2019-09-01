6d20d71c5edd1db8411a8dba35ca7549d67b4a9d
natj <nattila.joonas@gmail.com>
2019-09-01 21:50:57 +0200

    add MPI extent check, it was needed

--- pic/communicate.h
+++ pic/communicate.h
@@ -55,6 +55,28 @@ inline MPI_Datatype get_mpi_datatype<::pic::Particle>(
       &obj_type);
 
   MPI_Type_commit(&obj_type);
+
+
+  /* check extent (not really necessary on most platforms) */
+  MPI_Aint lb, extent, dist[2];
+  MPI_Type_get_extent(obj_type, &lb, &extent);
+  std::array<pic::Particle,10> particles;
+
+  MPI_Get_address(&particles[0], &dist[0]);
+  MPI_Get_address(&particles[1], &dist[1]);
+
+  MPI_Datatype temptype;
+  if (extent != (dist[1] - dist[0])) {
+    std::cout << "WARNING! Extra padding between arrays in MPI datatype\n";
+    temptype = obj_type;
+    lb = 0;
+    extent = dist[1] - dist[0];
+
+    MPI_Type_create_resized(temptype, lb, extent, &obj_type);
+    MPI_Type_commit(&obj_type);
+    MPI_Type_free(&temptype);
+  }
+
   return obj_type;
 }
 
