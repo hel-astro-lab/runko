b99e225822a26117c526189ceb6b66d6e196c902
natj <nattila.joonas@gmail.com>
2019-10-24 13:42:11 +0200

    tegner modules

--- archs/tegner.sh
+++ archs/tegner.sh
@@ -4,9 +4,6 @@ module load gcc/6.2.0
 module load hdf5/1.10.0-gcc-6.2
 module load anaconda/py35/4.2.0
 
-#module unload fftw/3.3.7-gcc-7.2-openmpi-3.0-single
-#module load fftw/3.3.4-gcc-8.4-openmpi-1.8-double
-#module load fftw/3.3.4-gcc-8.4-openmpi-1.8-single
 module load fftw/3.3.7-gcc-7.2-openmpi-3.0-single
 module load fftw/3.3.7-gcc-7.2-openmpi-3.0-double
 
@@ -25,13 +22,10 @@ export PYTHONPATH=$PYTHONPATH:$RUNKODIR/corgi/lib
 export PYTHONPATH=$PYTHONPATH:$RUNKODIR/python
 export PYTHONPATH=$PYTHONPATH:$RUNKODIR/analysis
 
+
+#extra directory in case one has manually installed python packages in $LUSTREDIR/lib
+export PYTHONPATH=$PYTHONPATH:$LUSTREDIR/lib/python3.6/site-packages
+
 export PYTHONDONTWRITEBYTECODE=true
 export HDF5_USE_FILE_LOCKING=FALSE
 
-
-# Tegner does not load all modules automatically (why?!) so we do this by hand
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/pdc/vol/fftw/3.3.4/gcc/8.4/openmpi/1.8/single/lib
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/pdc/vol/fftw/3.3.4/gcc/8.4/openmpi/1.8/double/lib
-
-#export PATH=$PATH:/pdc/vol/fftw/3.3.4/gcc/8.4/openmpi/1.8/single/include
-#export PATH=$PATH:/pdc/vol/fftw/3.3.4/gcc/8.4/openmpi/1.8/double/include
