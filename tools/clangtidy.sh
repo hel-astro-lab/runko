2bae589e25dc3409b30d2eeb57ce897ecb1a2aa3
natj <nattila.joonas@gmail.com>
2020-02-24 22:22:18 +0000

    udpate clang tidy script

--- tools/clangtidy.sh
+++ tools/clangtidy.sh
@@ -9,25 +9,9 @@ python3 tools/run-clang-tidy.py -p=build/ \
 	-header-filter=/home/natj/runko \
 	-fix \
 	-checks=-*,\
-modernize-avoid-bind,\
-modernize-concat-nested-namespaces,\
-modernize-deprecated-headers,\
-modernize-deprecated-ios-base-aliases,\
-modernize-make-shared,\
-modernize-make-unique,\
-modernize-pass-by-value,\
-modernize-raw-string-literal,\
-modernize-redundant-void-arg,\
-modernize-replace-auto-ptr,\
-modernize-replace-random-shuffle
-#modernize-return-braced-init-list,\
-#modernize-shrink-to-fit,\
-#modernize-unary-static-assert,\
-#modernize-use-auto,\
-#modernize-use-default-member-init,\
-#modernize-use-bool-literals,\
-#modernize-use-emplace,\
-#modernize-use-equals-default,\
+modernize-use-emplace,\
+modernize-use-equals-default,\
+
 #modernize-use-equals-delete,\
 #modernize-use-nodiscard,\
 #modernize-use-noexcept
@@ -36,17 +20,27 @@ modernize-replace-random-shuffle
 
 #checks not to be run
 # modernize-loop-convert
-
-
 # modernize-pass-by-value
 # modernize-use-equals-default
 # modernize-use-default-member-init
 # modernize-use-using
 
-# modernize-deprecated-headers
-	#-checks=-*,modernize-*,-modernize-use-trailing-return-type 
-
-
-	#-checks=-*,modernize-avoid-bind
+# checks already run
+#modernize-avoid-bind,\
+#modernize-concat-nested-namespaces,\
+#modernize-deprecated-headers,\
+#modernize-deprecated-ios-base-aliases,\
+#modernize-make-shared,\
+#modernize-make-unique,\
+#modernize-raw-string-literal,\
+#modernize-redundant-void-arg,\
+#modernize-replace-auto-ptr,\
+#modernize-replace-random-shuffle,\
+#modernize-return-braced-init-list,\
+#modernize-shrink-to-fit,\
+#modernize-unary-static-assert,\
+#modernize-use-auto,\
+#modernize-use-default-member-init,\
+#modernize-use-bool-literals,\
 
 
