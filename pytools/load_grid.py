4f63b02dfc57d8bde6d2adfee2b84cc484531705
natj <nattila.joonas@gmail.com>
2020-05-13 14:01:15 -0400

    save mpi grid state to disk

--- pytools/load_grid.py
+++ pytools/load_grid.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*- 
 
 import numpy as np
+import h5py as h5
 
 import pycorgi
 import pyrunko
@@ -17,7 +18,7 @@ def balance_mpi(n, conf, comm_size=None):
 # load nodes using 2D Hilbert curve
 def balance_mpi_2D(n, comm_size=None):
 
-    if n.master:  # only master initializes; then sends
+    if n.rank() == 0:  # only master initializes; then sends
         if comm_size == None:
             comm_size = n.size()
 
@@ -70,7 +71,7 @@ def balance_mpi_2D(n, comm_size=None):
 # load nodes using 3D Hilbert curve
 def balance_mpi_3D(n, comm_size=None):
 
-    if n.master:  # only master initializes; then sends
+    if n.rank() == 0:  # only master initializes; then sends
         if comm_size == None:
             comm_size = n.size()
 
@@ -131,7 +132,7 @@ def balance_mpi_3D(n, comm_size=None):
 #make random starting order
 def load_mpi_randomly(n):
     np.random.seed(0)
-    if n.master:
+    if n.rank() == 0:
         for i in range(n.get_Nx()):
             for j in range(n.get_Ny()):
                 val = np.random.randint( n.size() )
@@ -141,8 +142,8 @@ def load_mpi_randomly(n):
 
 
 #load nodes to be in stripe formation (splitted in X=horizontal direction)
-def load_mpi_x_strides(n):
-    if n.master: #only master initializes; then sends
+def load_mpi_x_strides(n, conf):
+    if n.rank() == 0: #only master initializes; then sends
         stride = np.zeros( (n.get_Nx()), np.int64)
         dx = np.float(n.get_Nx()) / np.float(n.size() ) 
         for i in range(n.get_Nx()):
@@ -158,10 +159,10 @@ def load_mpi_x_strides(n):
 
 
 #load nodes to be in stripe formation (splitted in Y=vertical direction)
-def load_mpi_y_strides(n):
-    if n.master: #only master initializes; then sends
+def load_mpi_y_strides(n, conf):
+    if n.rank() == 0: #only master initializes; then sends
         stride = np.zeros( (n.get_Ny()), np.int64)
-        dy = np.float(n.get_Ny()) / np.float(n.Nrank) 
+        dy = np.float(n.get_Ny()) / np.float(n.size() ) 
         for j in range(n.get_Ny()):
             val = np.int( j/dy )
             stride[j] = val
@@ -174,6 +175,76 @@ def load_mpi_y_strides(n):
     return
 
 
+# get mpi grid from the grid object
+# NOTE: every rank returns their version. In general this should be the
+# same for all because grids are kept up-to-date with mpi broadcasting from
+# the master rank.
+def get_mpi_grid(grid, conf):
+    nx = grid.get_Nx()
+    ny = grid.get_Ny()
+    nz = grid.get_Ny()
+
+    mpi_grid = np.zeros((nx, ny, nz), np.int64)
+    for i in range(nx):
+        for j in range(ny):
+            for k in range(nz):
+                if conf.twoD:
+                    val = grid.get_mpi_grid(i, j)
+                elif conf.threeD:
+                    val = grid.get_mpi_grid(i, j, k)
+
+                mpi_grid[i,j,k] = val
+
+    return mpi_grid
+
+
+# multi-rank call to get tile ids from each rank; 
+# NOTE: when ran with multiple ranks this only returns the local
+#       list of tile ids.
+def get_tile_id_grid(grid, conf):
+
+    # get tile ids from everybody
+    nx = grid.get_Nx()
+    ny = grid.get_Ny()
+    nz = grid.get_Nz()
+    tile_grid = np.zeros( (nx,ny,nz), np.int64 )
+
+    for cid in grid.get_tile_ids():
+        tile = grid.get_tile( cid )
+
+        if conf.twoD:
+            (i, j) = tile.index
+            k = 0
+        elif conf.threeD:
+            (i, j, k) = tile.index
+
+        # test that there are no error tiles
+        assert cid+1 > 0
+
+        # finally, set id to correct location
+        tile_grid[i,j,k] = cid + 1
+
+    #TODO gather back to master info from all ranks
+
+    return tile_grid
+
+
+
+# save a snapshot of the mpi tile grid configuration to disk as h5 file
+def save_mpi_grid_to_disk(outdir, lap, grid, conf):
+
+    # get tile id grid; need to call with every rank
+    #tid_grid = get_tile_id_grid(grid, conf)
+
+    if grid.rank() == 0: #only master saves rank performs this
+        mpi_grid = get_mpi_grid(grid, conf)
+        fname = outdir + '/mpi_{}.h5'.format(str(lap)) #.rjust(4,'0'))
+        f5 = h5.File(fname,'w')
+
+        dset1 = f5.create_dataset('mpi_grid',  data=mpi_grid)
+        #dset2 = f5.create_dataset('tile_grid', data=tid_grid)
+
+        f5.close()
 
 
 
