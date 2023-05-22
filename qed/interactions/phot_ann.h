cc94615f24f9dc9185bdf5260f807f5678381245
natj <nattila.joonas@gmail.com>
2023-05-22 19:39:41 -0400

    phot ann

---
+++ qed/interactions/phot_ann.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "interaction.h"
+
+
+namespace qed {
+  using std::string;
+  using std::tuple;
+
+
+class PhotAnn :
+  public Interaction
+{
+public:
+
+  using Interaction::Interaction;
+
+  tuple<float_p, float_p> get_minmax_ene( string t1, string t2 ) override;
+
+  float_p comp_cross_section(
+    string t1, float_p ux1, float_p uy1, float_p uz1,
+    string t2, float_p ux2, float_p uy2, float_p uz2) override;
+
+  void interact(
+        string& t1, float_p& ux1, float_p& uy1, float_p& uz1,
+        string& t2, float_p& ux2, float_p& uy2, float_p& uz2) override;
+
+
+}; // end of PhotAnn class
+
+} // end of namespace qed
+
