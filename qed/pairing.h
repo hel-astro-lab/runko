949a026d5246a34d5b0cbe108cad74c7c9a16fa9
natj <nattila.joonas@gmail.com>
2023-06-01 18:22:56 -0600

    add empty check

--- qed/pairing.h
+++ qed/pairing.h
@@ -39,8 +39,11 @@ public:
   //using Tile_map = std::unordered_map<TileID_t, Tileptr>;
   //Tile_map tiles; /// Map with tile_id & tile data
 
+  std::vector<InteractionPtr> interactions;
 
-  // add interactions to internal memory; done via pointers
+
+  // add interactions to internal memory of the class; 
+  // done via pointers to handle pybind interface w/ python
   void add_interaction(InteractionPtr iptr)
   {
     assert(iptr); // check that we are not appending nullptr
@@ -49,9 +52,20 @@ public:
     auto t1 = iptr->t1;
     auto t2 = iptr->t2;
 
-    std::cout << " adding:" << name << " of t1/t2 " << t1 << " " << t2 << std::endl;
+    std::cout << " adding: " << name << " of t1/t2 " << t1 << " " << t2 << std::endl;
+    interactions.push_back(iptr);
   }
 
+  // check if interaction list is empty for type t1
+  bool is_empty(string& t1) 
+  {
+    int i=0;
+    for(auto iptr : interactions){
+      if(t1 == iptr->t1) i += 1;
+    }
+
+    return (i > 0) ? false : true; 
+  }
 
   template<size_t D>
   void solve(pic::Tile<D>& tile)
@@ -59,11 +73,9 @@ public:
     for(auto&& con1 : tile.containers)
     {
       auto t1 = con1.type;
+      if(is_empty(t1)) continue; // no interactions with incident type t1
+
       std::cout << "container type:" << t1 << std::endl;
-      
-
-
-
     }
   }
 
