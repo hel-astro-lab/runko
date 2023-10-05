c87823d3c8e07054ced7d8093415efa624d4e649
natj <nattila.joonas@gmail.com>
2023-10-05 11:35:36 -0400

    stride option to h5 reading

--- pytools/iotools.py
+++ pytools/iotools.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 # read simulation output file and reshape to python format
-def read_h5_array(f5, var_name):
+def read_h5_array(f5, var_name, stride=1):
 
     try:
         nx = f5['Nx'][()]
@@ -10,12 +10,19 @@ def read_h5_array(f5, var_name):
         nz = f5['Nz'][()]
     except:
         nx,ny,nz = np.shape(f5[var_name])
+        print("read_h5_array: fallback to brute-force reading...")
         val = f5[var_name][()]
         return val
 
     # column-ordered data with image convention (x horizontal, y vertical, ..)
     # i.e., so-called fortran ordering
-    val = f5[var_name][()]
+    if stride == 1:
+        val = f5[var_name][()]
+    else:
+        val = f5[var_name][::stride**3]
+        nx = int(nx/stride)
+        ny = int(ny/stride)
+        nz = int(nz/stride)
 
     #print("reshaping 1D array of {} into multiD with {} {} {}".format(len(val), nx,ny,nz))
 
