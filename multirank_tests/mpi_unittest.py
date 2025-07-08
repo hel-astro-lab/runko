611a42627e3d7642454cd1140640e66671c1a8c1
Miro Palmu <email@miropalmu.cc>
2025-07-08 15:41:05 +0300

    tests: implement deferred multirank tests

--- multirank_tests/mpi_unittest.py
+++ multirank_tests/mpi_unittest.py
@@ -3,23 +3,16 @@ Nano unittest library for MPI related tests.
 """
 
 from mpi4py import MPI
+from dataclasses import dataclass
 
 _comm = MPI.COMM_WORLD
 _comm_size = _comm.Get_size()
 _rank = _comm.Get_rank()
 
-def assertEqual(a, b):
-    """
-    Asserts a == b on every rank.
-    """
 
-    ok = a == b
+def _gather_error_msgs(msg: str):
+    msgs = _comm.gather(msg, root=0)
 
-    err_msg = ""
-    if not ok:
-        err_msg = f"rank {_rank}: {a} != {b}\n"
-
-    msgs = _comm.gather(err_msg, root=0)
     if _rank == 0:
         concatted_msg = "\n"
         passes = True
@@ -30,3 +23,74 @@ def assertEqual(a, b):
         if not passes:
             raise RuntimeError(concatted_msg)
 
+
+def assertEqual(a, b) -> None:
+    """
+    Asserts a == b on every rank.
+
+    Has to be called on every rank.
+    """
+    ok = a == b
+
+    err_msg = ""
+    if not ok:
+        err_msg = f"rank {_rank}: {a} != {b}\n"
+
+    _gather_error_msgs(err_msg)
+
+def assertNotEqual(a, b) -> None:
+    """
+    Asserts a != b on every rank.
+
+    Has to be called on every rank.
+    """
+    ok = a != b
+
+    err_msg = ""
+    if not ok:
+        err_msg = f"rank {_rank}: {a} == {b}\n"
+
+    _gather_error_msgs(err_msg)
+
+
+@dataclass
+class DeferredAssertResult:
+    """
+    Represents result of deferred assertion result.
+    This means that functions returning objects of this class
+    do not yet assert on failed test, but store the result in the object.
+
+    These can be checked collectively with assertDeferredResults,
+    which will sync accross ranks and assert that they are correct.
+    """
+
+    error_msg: str = ""
+
+    def fail(self) -> bool:
+        return 0 != len(self.error_msg)
+
+
+def assertEqualDeferred(a, b) -> DeferredAssertResult:
+    """
+    Similar to assertEqual but,
+    the checking is deferred to assertDeferredResults.
+    """
+
+    if a == b:
+        return DeferredAssertResult("")
+    else:
+        return DeferredAssertResult(f"rank {_rank}: {a} != {b}\n")
+
+
+def assertDeferredResults(results: list[DeferredAssertResult]) -> None:
+    """
+    Assert that all given results pass on every rank,
+    i.e. has to be called on every rank.
+    """
+
+    concatted_err_msg = ""
+
+    for result in results:
+        concatted_err_msg += result.error_msg
+
+    _gather_error_msgs(concatted_err_msg)
--- multirank_tests/test_testing_setup.py
+++ multirank_tests/test_testing_setup.py
@@ -1,4 +1,11 @@
 import mpi_unittest
+import random
 
 if __name__ == "__main__":
     mpi_unittest.assertEqual(45, 45)
+
+    results = []
+    for i in range(random.randint(0, 10)):
+        results.append(mpi_unittest.assertEqualDeferred(int, int))
+
+    mpi_unittest.assertDeferredResults(results)
