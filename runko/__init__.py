64672216c20da48a0bef10f77d624139bfccc627
Miro Palmu <email@miropalmu.cc>
2025-07-15 12:56:37 +0300

    feat(runko-module): implement on_main_rank

--- runko/__init__.py
+++ runko/__init__.py
@@ -4,3 +4,20 @@ from .TileGrid import TileGrid
 import pyrunko.emf2.threeD as emf
 from .Simulation import Simulation
 from pyrunko.tools import comm_mode
+
+
+def on_main_rank() -> bool:
+    """
+    Checks if the caller is "main" rank.
+
+    There is no other guarantees other that there is only one main rank.
+    """
+
+    try:
+        from mpi4py import MPI
+        return MPI.COMM_WORLD.Get_rank() == 0
+    except:
+        # If importing mpi4py fails, assume that there is no MPI
+        # which means that there has to be only one rank.
+        return True
+
