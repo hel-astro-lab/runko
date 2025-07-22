151d560ad84f1b769338b31902afbfca7cba1aca
Miro Palmu <email@miropalmu.cc>
2025-07-22 17:20:00 +0300

    refactor(tools): modernize {Vec,Mat}{3,4} classes

--- tools/vector.h
+++ tools/vector.h
@@ -1,366 +1,305 @@
 #pragma once
 
-#include <iostream>
+#include <functional>
+// tyvi has missing include of <functional> :(
+
+#include "tyvi/mdspan.h"
+
 #include <array>
 #include <cmath>
+#include <concepts>
+#include <iostream>
+#include <type_traits>
+#include <utility>
 
 namespace toolbox {
 
-  using std::ostream;
+template<std::floating_point T, std::size_t D>
+struct VecD {
+  std::array<T, D> data {};
 
-template <typename T> 
-class Vec3 
-{
-public:
-
-  std::array<T, 3> data;
-
-  inline T& operator()(size_t ind) { 
-    return data[ ind ];
-  }
-
-  inline const T& operator()(size_t ind) const { 
-    return data[ ind ];
-  }
-
-  Vec3() {};
-
-  Vec3(T x, T y, T z) 
+  template<typename Self>
+  constexpr auto& operator()(this Self& self, const std::size_t ind)
   {
-    data[0] = x;  
-    data[1] = y;  
-    data[2] = z;  
+    return self.data[ind];
   }
 
-  Vec3(std::array<T,3>& arr)
+  template<typename Self>
+  constexpr auto& operator[](this Self& self, const std::size_t ind)
   {
-    data[0] = arr[0];  
-    data[1] = arr[1];  
-    data[2] = arr[2];  
+    return self.data[ind];
   }
 
+  constexpr VecD() = default;
 
-  inline void set(T x, T y, T z){
-    data[0] = x;  
-    data[1] = y;  
-    data[2] = z;  
+  template<std::convertible_to<T>... U>
+    requires(sizeof...(U) == D)
+  constexpr VecD(U&&... args)
+  {
+    data = std::array<T, D> { static_cast<T>(std::forward<U>(args))... };
   }
 
-  friend ostream& operator<<(ostream& os, Vec3<T>& v)
+  constexpr VecD(const std::array<T, D>& arr) { data = arr; }
+
+  template<std::convertible_to<T>... U>
+    requires(sizeof...(U) == D)
+  constexpr void set(U&&... args)
+  {
+    *this = VecD(std::forward<U>(args)...);
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const VecD<T, D>& v)
   {
     os << "[";
-    for(size_t i=0; i < 3; i++) os << v(i) << " ";
+    for(size_t i = 0; i < D; i++) os << v(i) << " ";
     os << "]";
-    return os; 
+    return os;
   }
+};
 
-}; // end of Vec3 class
 
+template<std::floating_point T>
+using Vec3 = VecD<T, 3>;
 
-template <typename T> 
-class Vec4 
-{
-  public:
+template<std::floating_point T>
+using Vec4 = VecD<T, 4>;
 
-    std::array<T, 4> data;
+/// Deduction guide to make `Vec3 v(1, 2, 3);` work.
+template<typename... U>
+VecD(U&&...) -> VecD<std::common_type_t<U...>, sizeof...(U)>;
 
-    inline T& operator()(size_t ind) { 
-      return data[ ind ];
-    }
+template<std::floating_point T, std::size_t D>
+  requires(D >= 2)
+struct MatD {
+  std::array<T, D * D> data {};
 
-    inline const T& operator()(size_t ind) const { 
-      return data[ ind ];
-    }
-
-    Vec4() {};
-
-    Vec4(T x, T y, T z, T w) 
-    {
-      data[0] = x;  
-      data[1] = y;  
-      data[2] = z;  
-      data[3] = w;  
-    }
-
-  friend ostream& operator<<(ostream& os, const Vec4<T>& v)
+  /// Column major order.
+  constexpr auto mds(this auto& self)
   {
-    os << "[";
-    for(size_t i=0; i < 4; i++) os << v(i) << " ";
-    os << "]";
-    return os; 
-  }
-}; // end of Vec4 class
-
-
-template <typename T> 
-class Mat3
-{
-public:
-
-  std::array<T, 9> data;
-
-  inline T& operator()(size_t i, size_t j) { 
-    return data[ j + 3*i ]; // NOTE: same ordering as in numpy
+    static constexpr bool is_const =
+      std::is_const_v<std::remove_reference_t<decltype(self)>>;
+    using TT = std::conditional_t<is_const, const T, T>;
+    return tyvi::sstd::geometric_mdspan<TT, 2, D, std::layout_left>(self.data.data());
   }
 
-  inline const T& operator()(size_t i, size_t j) const { 
-    return data[ j + 3*i ]; 
-  }
-
-  Mat3() {};
-
-  Mat3( Vec3<T>& x, Vec3<T>& y, Vec3<T>& z)
+  /// Column major order.
+  constexpr auto& operator[](this auto& self, const size_t i, const size_t j)
   {
-    data[0] = x(0);  
-    data[1] = x(1);  
-    data[2] = x(2);  
-
-    data[3] = y(0);  
-    data[4] = y(1);  
-    data[5] = y(2);  
-
-    data[6] = z(0);  
-    data[7] = z(1);  
-    data[8] = z(2);  
+    return self.mds()[i, j];
   }
 
-  friend ostream& operator<<(ostream& os, const Mat3<T>& M)
+  /// Column major order.
+  constexpr auto& operator()(this auto& self, const size_t i, const size_t j)
+  {
+    return self.mds()[i, j];
+  }
+
+  constexpr MatD() = default;
+
+  template<std::same_as<VecD<T, D>>... U>
+    requires(sizeof...(U) == D)
+  constexpr MatD(const U&... x)
+  {
+    const auto vecs = std::array { x... };
+    const auto m    = this->mds();
+    for(const auto [i, j]: tyvi::sstd::geometric_index_space<2, D>()) {
+      m[i, j] = vecs[i](j);
+    }
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const MatD<T, D>& M)
   {
     os << std::endl;
     os << "[";
-    os << "[ "  << M(0, 0) << " , " << M(0, 1) << " , " << M(0, 2) << "]" << std::endl;
-    os << " [ " << M(1, 0) << " , " << M(1, 1) << " , " << M(1, 2) << "]" << std::endl;
-    os << " [ " << M(2, 0) << " , " << M(2, 1) << " , " << M(2, 2) << "]";
+    for(auto i = 0uz; i < D; ++i) {
+      os << "[ ";
+      for(auto j = 0uz; i < D - 1; ++i) { os << M(i, j) << " , "; }
+      os << M(i, D - 1) << "]" << "\n";
+    }
     os << "]" << std::endl;
 
-    return os; 
+    return os;
   }
+};
 
-}; // end of 3 class
+template<std::floating_point T>
+using Mat3 = MatD<T, 3>;
 
-
-template <typename T> 
-class Mat4
-{
-  public:
-
-    std::array<T, 16> data;
-
-    inline T& operator()(size_t i, size_t j) { 
-      return data[ j + 4*i ]; // NOTE: same ordering as in numpy
-    }
-
-    inline const T& operator()(size_t i, size_t j) const { 
-      return data[ j + 4*i ]; 
-    }
-
-    Mat4() {};
-
-    Mat4( Vec4<T>& x, Vec4<T>& y, Vec4<T>& z, Vec4<T>& w)
-    {
-      data[0]  = x(0);  
-      data[1]  = x(1);  
-      data[2]  = x(2);  
-      data[3]  = x(3);  
-
-      data[4]  = y(0);  
-      data[5]  = y(1);  
-      data[6]  = y(2);  
-      data[7]  = y(3);  
-
-      data[8]  = z(0);  
-      data[9]  = z(1);  
-      data[10] = z(2);  
-      data[11] = z(3);  
-
-      data[12] = w(0);  
-      data[13] = w(1);  
-      data[14] = w(2);  
-      data[15] = w(3);  
-    }
-
-  friend ostream& operator<<(ostream& os, const Mat4<T>& M)
-  {
-    os << std::endl;
-    os << "[";
-    os << "[ "  << M(0, 0) << " , " << M(0, 1) << " , " << M(0, 2) << " , " << M(0,3) << "]" << std::endl;
-    os << " [ " << M(1, 0) << " , " << M(1, 1) << " , " << M(1, 2) << " , " << M(1,3) << "]" << std::endl;
-    os << " [ " << M(2, 0) << " , " << M(2, 1) << " , " << M(2, 2) << " , " << M(2,3) << "]" << std::endl;
-    os << " [ " << M(3, 0) << " , " << M(3, 1) << " , " << M(3, 2) << " , " << M(3,3) << "]";
-    os << "]" << std::endl;
-
-    return os; 
-  }
-
-}; // end of Mat3 class
-
+template<std::floating_point T>
+using Mat4 = MatD<T, 4>;
 
 //--------------------------------------------------
 // arithmetics
 // elementwise arithmetics
-  
-template <class T>
-inline Vec3<T> operator/(Vec3<T>& v, T s)  // NOTE vector passed as reference
+
+template<std::floating_point T, std::size_t D>
+constexpr VecD<T, D>
+  operator/(const VecD<T, D>& v, const T s)
 {
-  Vec3<T> ret;
-  for(size_t i=0; i<3; i++) ret(i) = v(i)/s;
+  VecD<T, D> ret;
+  for(size_t i = 0; i < D; i++) ret(i) = v(i) / s;
+  return ret;
+}
+
+template<std::floating_point T, std::size_t D>
+constexpr VecD<T, D>
+  operator*(const VecD<T, D>& v, const T s)
+{
+  VecD<T, D> ret;
+  for(size_t i = 0; i < D; i++) ret(i) = v(i) * s;
+  return ret;
+}
+
+template<std::floating_point T, std::size_t D>
+constexpr VecD<T, D>
+  operator*(const T s, const VecD<T, D>& v)
+{
+  return v * s;
+}
+
+
+template<std::floating_point T, std::size_t D>
+constexpr VecD<T, D>
+  operator+(const VecD<T, D>& v1, const VecD<T, D>& v2)
+{
+  VecD<T, D> ret;
+  for(size_t i = 0; i < D; i++) ret(i) = v1(i) + v2(i);
   return ret;
 }
 
 
-template <class T>
-inline Vec3<T> operator*(T s, Vec3<T>& v) 
+template<std::floating_point T, std::size_t D>
+constexpr VecD<T, D>
+  operator-(const VecD<T, D>& v1, const VecD<T, D>& v2)
 {
-  Vec3<T> ret;
-  for(size_t i=0; i<3; i++) ret(i) = v(i)*s;
+  VecD<T, D> ret;
+  for(size_t i = 0; i < D; i++) ret(i) = v1(i) - v2(i);
   return ret;
 }
 
-template <class T>
-inline const Vec3<T> operator*(const T s, const Vec3<T>& v) 
+template<std::floating_point T, std::size_t D>
+constexpr T
+  dot(const VecD<T, D>& v1, const VecD<T, D>& v2)
 {
-  Vec3<T> ret;
-  for(size_t i=0; i<3; i++) ret(i) = v(i)*s;
+  T ret { 0 };
+  for(size_t i = 0; i < D; i++) ret += v1(i) * v2(i);
   return ret;
 }
 
-
-template <class T>
-inline Vec3<T> operator+(Vec3<T>& v1, Vec3<T>& v2) 
+template<std::floating_point T, std::size_t D>
+constexpr T
+  sum(const VecD<T, D>& v)
 {
-  Vec3<T> ret;
-  for(size_t i=0; i<3; i++) ret(i) = v1(i) + v2(i);
-  return ret;
-}
-
-
-template <class T>
-inline Vec3<T> operator-(Vec3<T>& v1, Vec3<T>& v2) 
-{
-  Vec3<T> ret;
-  for(size_t i=0; i<3; i++) ret(i) = v1(i) - v2(i);
-  return ret;
-}
-
-
-template <class T>
-inline T dot(Vec3<T>& v1, Vec3<T>& v2) 
-{
-  T ret = 0.0;
-  for(size_t i=0; i<3; i++) ret += v1(i)*v2(i);
-  return ret;  
-}
-
-template <class T>
-inline T dot(Vec4<T>& v1, Vec4<T>& v2) 
-{
-  T ret = 0.0;
-  for(size_t i=0; i<4; i++) ret += v1(i)*v2(i);
-  return ret;  
-}
-
-template <class T>
-inline T sum(Vec3<T>& v)
-{
-  return v(0) + v(1) + v(2);
+  // Ugly syntax until C++26 :(
+  return [&]<std::size_t... I>(std::index_sequence<I...>) {
+    return (v(I) + ...);
+  }(std::make_index_sequence<D>());
 }
 
 // matrix-vector product
-template <class T>
-inline Vec3<T> dot(Mat3<T>& M, Vec3<T>& v) 
+template<std::floating_point T, std::size_t D>
+constexpr VecD<T, D>
+  dot(const MatD<T, D>& M, const VecD<T, D>& v)
 {
-  Vec3<T> ret;
-  ret(0) = v(0)*M(0,0) + v(1)*M(0,1) + v(2)*M(0,2);
-  ret(1) = v(0)*M(1,0) + v(1)*M(1,1) + v(2)*M(1,2);
-  ret(2) = v(0)*M(2,0) + v(1)*M(2,1) + v(2)*M(2,2);
-  return ret;
-}
+  VecD<T, D> ret;
 
-// matrix-vector product
-template <class T>
-inline Vec4<T> dot(Mat4<T>& M, Vec4<T>& v) 
-{
-  Vec4<T> ret;
-  ret(0) = v(0)*M(0,0) + v(1)*M(0,1) + v(2)*M(0,2) + v(3)*M(0,3);
-  ret(1) = v(0)*M(1,0) + v(1)*M(1,1) + v(2)*M(1,2) + v(3)*M(1,3);
-  ret(2) = v(0)*M(2,0) + v(1)*M(2,1) + v(2)*M(2,2) + v(3)*M(2,3);
-  ret(3) = v(0)*M(3,0) + v(1)*M(3,1) + v(2)*M(3,2) + v(3)*M(3,3);
+  for(const auto [i, j]: tyvi::sstd::geometric_index_space<2, D>()) {
+    ret[i] += M[i, j] * v[j];
+  }
   return ret;
 }
 
 
-template <class T>
-inline Vec3<T> cross(Vec3<T>& v1, Vec3<T>& v2) 
+template<std::floating_point T>
+constexpr Vec3<T>
+  cross(const Vec3<T>& v1, const Vec3<T>& v2)
 {
   Vec3<T> ret;
-  ret(0) =  v1(1)*v2(2) - v1(2)*v2(1);
-  ret(1) = -v1(0)*v2(2) + v1(2)*v2(0);
-  ret(2) =  v1(0)*v2(1) - v1(1)*v2(0);
+  ret(0) = v1(1) * v2(2) - v1(2) * v2(1);
+  ret(1) = -v1(0) * v2(2) + v1(2) * v2(0);
+  ret(2) = v1(0) * v2(1) - v1(1) * v2(0);
   return ret;
 }
 
 // unit vector into direction of v1 x v2
-template <class T>
-inline Vec3<T> unit_cross(Vec3<T>& v1, Vec3<T>& v2) 
+template<class T>
+inline Vec3<T>
+  unit_cross(Vec3<T>& v1, Vec3<T>& v2)
 {
-  Vec3<T> ret = cross(v1, v2);
+  const Vec3<T> ret = cross(v1, v2);
   // TODO prevent dividing by zero
-  return ret/norm(ret);
+  return ret / norm(ret);
 }
 
 
 // matrix inversion
-template <class T>
-inline Mat3<T> inv(Mat3<T>& M) 
+template<std::floating_point T>
+constexpr Mat3<T>
+  inv(const Mat3<T>& M)
 {
   Mat3<T> ret;
 
   T det = 0.0;
-  for(int i=0; i<3; i++) det += (M(0,i) * ( M(1,(i+1)%3) * M(2,(i+2)%3) - M(1,(i+2)%3) * M(2,(i+1)%3) ));
+  for(int i = 0; i < 3; i++)
+    det +=
+      (M(0, i) *
+       (M(1, (i + 1) % 3) * M(2, (i + 2) % 3) - M(1, (i + 2) % 3) * M(2, (i + 1) % 3)));
 
-  for(int i = 0; i<3; i++) {
-  for(int j = 0; j<3; j++) {
-    ret(i,j) = ( (M((j+1)%3,(i+1)%3) * M((j+2)%3,(i+2)%3) ) - (M((j+1)%3,(i+2)%3) * M( (j+2)%3,(i+1)%3)) )/ det;
-  }}
+  for(int i = 0; i < 3; i++) {
+    for(int j = 0; j < 3; j++) {
+      ret(i, j) = ((M((j + 1) % 3, (i + 1) % 3) * M((j + 2) % 3, (i + 2) % 3)) -
+                   (M((j + 1) % 3, (i + 2) % 3) * M((j + 2) % 3, (i + 1) % 3))) /
+                  det;
+    }
+  }
 
   return ret;
 }
 
 //--------------------------------------------------
 // vector norm
-inline float norm(float x, float y, float z)
+template<std::floating_point... T>
+constexpr std::common_type_t<T...>
+  norm(const T... args)
 {
-  return std::sqrt(x*x + y*y + z*z);
+  return std::sqrt(((args * args) + ...));
 }
 
-template <typename T> 
-inline T norm(Vec3<T>& v)
+template<std::floating_point T, std::size_t D>
+constexpr T
+  norm(const VecD<T, D>& v)
 {
-  return std::sqrt( v(0)*v(0) + v(1)*v(1) + v(2)*v(2) );
+  return std::sqrt(dot(v, v));
 }
 
-template <typename T> 
-inline T norm2d(Vec3<T>& v) // contracted norm in x y direction
+template<std::floating_point T, std::size_t D>
+constexpr T
+  norm2d(const VecD<T, D>& v)  // contracted norm in x y direction
 {
-  return std::sqrt( v(0)*v(0) + v(1)*v(1) );
+  return std::sqrt(v(0) * v(0) + v(1) * v(1));
 }
 
-template <typename T> 
-inline T norm1d(Vec3<T>& v) // contracted norm in x direction
+template<std::floating_point T, std::size_t D>
+constexpr T
+  norm1d(const VecD<T, D>& v)  // contracted norm in x direction
 {
-  return std::abs( v(0) );
+  return std::abs(v(0));
 }
 
-template <typename T> 
-inline T norm_minkowski(Vec4<T>& v)
+template<std::floating_point T, std::size_t D>
+constexpr T
+  norm_minkowski(VecD<T, D>& v)
 {
-  // NOTE: minkowski norm with -0 + 1 + 2 + 3 signature. However, we take absolute value
-  //       of the output to be agnostic about the metric signature.
-  return std::sqrt(std::abs( -v(0)*v(0) + v(1)*v(1) + v(2)*v(2) + v(3)*v(3) ) );
+  // NOTE: minkowski norm with -0 + 1 + 2 + 3 + ... signature. However, we take absolute
+  // value of the output to be agnostic about the metric signature.
+
+  // Ugly syntax until C++26 :(
+  const auto spatial_part = [&]<std::size_t... I>(std::index_sequence<I...>) {
+    return ((v(I + 1) * v(I + 1)) + ...);
+  }(std::make_index_sequence<D - 1>());
+
+  return std::sqrt(std::abs(-v(0) * v(0) + spatial_part));
 }
 
 
-
-
-
-} // enf of ns toolbox
+}  // namespace toolbox
