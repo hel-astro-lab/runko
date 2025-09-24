f3ab8f31ad5e5af8def9ffd9ca1be104c83f8e00
natj <nattila.joonas@gmail.com>
2025-09-24 17:04:44 +0300

    partially working hile installation script

---
+++ archs/hile-install.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+# Runko installation script for on the HILE cluster.
+
+set -v
+
+#-------------------------------------------------- 
+# Change current directory to the runko repository root
+cd "$(dirname "$0")"
+cd ../
+
+#-------------------------------------------------- 
+# Pre-download mdspan (because there is no internet connection on compute node)
+git clone https://github.com/kokkos/mdspan.git $HOME
+PRE_DOWNLOADED_MDSPAN_PATH="$HOME/mdspan"
+
+#-------------------------------------------------- 
+# standard cmake3 path in HILE (using lumi software stack)
+CMAKE3_PATH="/appl/lumi/SW/LUMI-24.03/common/EB/buildtools/24.03/bin/cmake"
+CMAKE="${CMAKE3_PATH} -DCPM_mdspan_SOURCE=${PRE_DOWNLOADED_MDSPAN_PATH} -DCMAKE_CXX_COMPILER=CC"
+
+#-------------------------------------------------- 
+# Update the PYTHONPATH environment variable with required runko
+#  and corgi-related paths in order to make our venv runko-aware::
+RUNKO_PATH=$(pwd)
+P1="$RUNKO_PATH/"
+P2="$RUNKO_PATH/lib"
+P3="$RUNKO_PATH/external/corgi/lib"
+export PYTHONPATH="$PYTHONPATH:$P1:$P2:$P3"
+
+#-------------------------------------------------- 
+# Load standard prerequisite modules for runko:
+module load PrgEnv-cray
+module load cray-hdf5
+module load craype-accel-amd-gfx90a
+module load craype-x86-milan
+module load cray-mpich 
+module load craype-network-ofi
+module load cray-python
+#module load rocm
+
+#-------------------------------------------------- 
+# Create a Python virtual environment specially for runko,
+# located within the runko repository:
+VENV_PATH="${RUNKO_PATH}/venvs"
+mkdir $VENV_PATH
+cd $VENV_PATH
+python -m venv runko-gpu
+cd $RUNKO_PATH
+
+# Load this runko virtual environment:
+source ${VENV_PATH}/runko-gpu/bin/activate
+
+# Install necessary Python dependencies
+pip3 install h5py scipy matplotlib numpy
+
+# Build MPI4PY:
+MPI4PY_BUILD_MPICC="cc -shared" python -m pip install --no-binary=mpi4py mpi4py
+
+
+#-------------------------------------------------- 
+# Next, we create a handy file which loads the necessary runko modules
+# whenever called with "source archs/runko-load-env":
+cat > archs/hile-load-runko-env << EOL
+# Tool to load runko modules
+# Usage: "source archs/hile-load-runko-env"
+# Load standard prerequisite modules for runko:
+
+module load PrgEnv-cray
+module load cray-hdf5
+module load craype-accel-amd-gfx90a
+module load craype-x86-milan
+module load cray-mpich 
+module load craype-network-ofi
+module load cray-python
+module load rocm
+
+# Load runko virtual environment:
+source ${VENV_PATH}/runko-gpu/bin/activate
+
+# Update the PYTHONPATH environment variable with runko path data
+export PYTHONPATH="\$PYTHONPATH:${P1}:${P2}:${P3}"
+export CMAKE=$CMAKE
+EOL
+
+
+#-------------------------------------------------- 
+## Build and unit test runko on 16 cores:
+## old version
+##$CMAKE3 -Bbuild -DCPM_mdspan_SOURCE=${PRE_DOWNLOADED_MDSPAN_PATH} -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=$CXX .
+
+#echo $CMAKE
+#$CMAKE -Bbuild -DCMAKE_BUILD_TYPE=Release .
+
+##make -j16 -Cbuild
+#
+## If all went well runko has now been compiled, and certain unit tests
+## should have automatically been executed and passed.
+#
