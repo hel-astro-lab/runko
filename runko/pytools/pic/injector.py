79875bdfa09e364d44ab353fdb5a592b301d7e1b
natj <nattila.joonas@gmail.com>
2025-09-25 16:26:32 +0300

    fix missing pic tools

---
+++ runko/pytools/pic/injector.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+
+import pyrunko.pic as pypic
+import numpy as np
+import sys, os
+from .tile_initialization import ind2loc
+
+
+# dummy function that returns always 1 (for weight initialization)
+def unit_w(xloc, ispcs, conf):
+    return 1.0
+
+
+# inject plasma into (individual) cells
+def inject(grid, 
+           vel_func, 
+           den_func, 
+           conf, 
+           align_species=True, 
+           w_func=unit_w,
+           ):
+    rank = grid.rank()
+
+    prtcl_tot = np.zeros(conf.Nspecies, dtype=np.int64)
+
+    # loop over all *local* cells
+    for i in range(grid.get_Nx()):
+        for j in range(grid.get_Ny()):
+            for k in range(grid.get_Nz()):
+
+                # check that we own this tile
+                if conf.threeD:
+                    pass_flag = grid.get_mpi_grid(i, j, k) == grid.rank()
+                elif conf.twoD:
+                    pass_flag = grid.get_mpi_grid(i, j) == grid.rank()
+                elif conf.oneD:
+                    pass_flag = grid.get_mpi_grid(i) == grid.rank()
+                if pass_flag:
+                    # print("creating ({},{})".format(i,j))
+
+                    # get cell & its content
+                    if conf.threeD: cid  = grid.id(i, j, k)
+                    elif conf.twoD: cid  = grid.id(i, j)
+                    elif conf.oneD: cid  = grid.id(i)
+
+                    tile = grid.get_tile(cid)  # get cell ptr
+
+                    # inject particles
+                    # even species are on their own; odd species are located on
+                    # top of previous even ones
+
+                    running_seed = 1
+                    for ispcs in range(conf.Nspecies):
+                        container = tile.get_container(ispcs)
+                        container.set_keygen_state(prtcl_tot[ispcs], rank)
+
+                        container.type = conf.prtcl_types[ispcs] # name container
+
+                        # open and read previously made particle species (for location reference)
+                        #if ispcs == 1:
+                        if align_species and conf.prtcl_types[ispcs] in ['e+', 'p']:
+                            #EPS = 1.0e-5
+                            ref_container = tile.get_container(0)
+                            xxs = ref_container.loc(0) #+ EPS*np.random.rand(1)
+                            yys = ref_container.loc(1) #+ EPS*np.random.rand(1)
+                            zzs = ref_container.loc(2) #+ EPS*np.random.rand(1)
+
+                            vxs = ref_container.vel(0)
+                            vys = ref_container.vel(1)
+                            vzs = ref_container.vel(2)
+
+                        ip_mesh = 0
+
+                        #tot_tiles = conf.Nx*conf.Ny*conf.Nz
+                        #np.random.seed(k*conf.Nx*conf.Ny + j*conf.Nx + i + ispcs*tot_tiles) # avoid re-starting the rng cycle
+
+                        #xD = 1.0 if conf.NxMesh > 1 else 0.0
+                        #yD = 1.0 if conf.NyMesh > 1 else 0.0
+                        #zD = 1.0 if conf.NzMesh > 1 else 0.0
+                        for n in range(conf.NzMesh):
+                            for m in range(conf.NyMesh):
+                                for l in range(conf.NxMesh):
+                                    # print(" sub mesh: ({},{},{})".format(l,m,n))
+                                    xloc = ind2loc((i, j, k), (l, m, n), conf)
+                                    # calculate how many prtcls per species to inject in this loc
+                                    ppc = den_func(xloc, ispcs, conf)
+                                    #TODO: pair loading factor
+                                    for ip in range(ppc):
+                                        x0, u0 = vel_func(xloc, ispcs, conf)
+                                        w = w_func(xloc, ispcs, conf)
+
+                                        # select position of previous species
+                                        if align_species and ispcs == 1:
+                                            xx = xxs[ip_mesh] #+ 1.0e-4 #*np.random.rand(1)
+                                            yy = yys[ip_mesh] #+ 1.0e-4 #*np.random.rand(1)
+                                            zz = zzs[ip_mesh] #+ 1.0e-4 #*np.random.rand(1)
+                                            x0 = [xx, yy, zz]  # overwrite location
+
+                                        #this step done in velocity function
+                                        #    xx = xloc[0] + np.random.rand(1)
+                                        #    yy = xloc[1] + np.random.rand(1)
+                                        #    zz = xloc[2] + np.random.rand(1)
+                                        #    x0 = [xx, yy, zz]  # overwrite location
+
+                                        # print("injecting particle sps={} of # {}:th to ({},{},{})".format(
+                                        #        ispcs, ip_mesh, x0[0], x0[1], x0[2]))
+                                        ip_mesh += 1
+
+                                        #--------------------------------------------------
+                                        # sanity checks
+                                        try:
+                                            x0 = np.array(x0).flatten().tolist()
+                                            u0 = np.array(u0).flatten().tolist()
+                                        except:
+                                            sys.exit()
+                                        if not(len(x0) == 3): sys.exit()
+                                        if not(len(u0) == 3): sys.exit()
+                                        if not(np.isscalar(w)): sys.exit()
+
+                                        #if (not( 0 < x0[0] < conf.NxMesh ) or 
+                                        #    not( 0 < x0[1] < conf.NyMesh ) or 
+                                        #    not( 0 < x0[2] < conf.NzMesh ) ):
+                                        #    print("ERROR inj:", ip, "x:", x0, " u:", u0, "w:", w)
+                                        #    print("NM", conf.NxMesh, conf.NyMesh, conf.NzMesh)
+                                        #    print("ijk", i,j,k)
+                                        #    print("lmn", l,m,n)
+                                        #    print("xloc", xloc)
+                                        #    sys.exit()
+                                        #--------------------------------------------------
+
+                                        container.add_particle(x0, u0, w)
+                                        prtcl_tot[ispcs] += 1
+
+                        # less noisy way of injecting particles
+                        #totp_per_tile = conf.NxMesh*conf.NyMesh*conf.NzMesh
+                        #xloc0 = ind2loc((i, j, k), (0, 0, 0), conf)
+                        #ppc = den_func(xloc0, ispcs, conf)
+                        #Nip = totp_per_tile*ppc #total number of prtcls of species in this tile
+
+                        #if align_species and ispcs > 0: #use prev container location
+                        #    xxps = np.array(xxs)
+                        #    yyps = np.array(yys)
+                        #    zzps = np.array(zzs)
+                        #else: #create new locations
+                        #    xxps = xloc0[0] + conf.NxMesh*np.random.rand(Nip)
+                        #    yyps = xloc0[1] + conf.NyMesh*np.random.rand(Nip)
+                        #    zzps = xloc0[2] + conf.NzMesh*np.random.rand(Nip)
+
+                        #for ip in range(Nip):
+                        #    x0 = [xxps[ip], yyps[ip], zzps[ip] ]
+                        #    _, u0 = vel_func(xloc0, ispcs, conf) #NOTE reading only the velocity
+
+                        #    ip_mesh += 1
+                        #    container.add_particle(x0, u0, 1.0)
+                        #    prtcl_tot[ispcs] += 1
+
+
+
+    # print("Injected total of:", prtcl_tot)
+    return prtcl_tot
---
+++ runko/pytools/pic/tile_initialization.py
@@ -0,0 +1,164 @@
+# -*- coding: utf-8 -*- 
+
+import numpy as np
+import pyrunko.pic as pypic
+
+
+def ind2loc(gridI, tileI, conf):
+
+    # grid coordinates
+    i, j, k = gridI
+    Nx = conf.Nx
+    Ny = conf.Ny
+    Nz = conf.Nz
+
+    # tile coordinates
+    l, m, n = tileI
+    NxMesh = conf.NxMesh
+    NyMesh = conf.NyMesh
+    NzMesh = conf.NzMesh
+
+    # grid spacing; start point + step
+    xmin = conf.xmin
+    ymin = conf.ymin
+    zmin = conf.zmin
+
+    dx = 1.0  # conf.dx
+    dy = 1.0  # conf.dy
+    dz = 1.0  # conf.dz
+    
+    # calculate coordinate extent
+    x = xmin + i * (NxMesh) * dx + l * dx
+    y = ymin + j * (NyMesh) * dy + m * dy
+    z = zmin + k * (NzMesh) * dz + n * dz
+
+    return [x, y, z]
+
+def initialize_tile(tile, indx, n, conf):
+
+    # set parameters
+    tile.cfl = conf.cfl
+    ppc = conf.ppc  # / conf.Nspecies
+
+    # load particle containers
+    for sps in range(conf.Nspecies):
+
+        if conf.threeD:
+            container = pypic.threeD.ParticleContainer()
+        elif conf.twoD:
+            container = pypic.twoD.ParticleContainer()
+        elif conf.oneD:
+            container = pypic.oneD.ParticleContainer()
+
+        # alternate injection between - and + charged prtcls
+        # mass is normalized to units of m_e
+        if sps == 0: # electron
+            container.q = -conf.qe
+            container.m = np.abs(conf.me) 
+        elif sps == 1: # positron
+            container.q = +conf.qe
+            container.m = np.abs(conf.me)
+        elif sps == 2: # photon
+            container.q = 0 
+            container.m = 0 
+        elif sps == 3: # proton
+            container.q = +conf.qe
+            container.m = np.abs(conf.mi)
+
+        # reserve memory for particles
+        Nprtcls = conf.NxMesh * conf.NyMesh * conf.NzMesh * conf.ppc
+        container.reserve(Nprtcls)
+
+        tile.set_container(container)
+
+    # set bounding box of the tile
+    mins = ind2loc(indx, (0, 0, 0), conf)
+    maxs = ind2loc(indx, (conf.NxMesh, conf.NyMesh, conf.NzMesh), conf)
+
+    #print("adding tile", indx, "minmax:", mins, "/", maxs)
+
+    if conf.threeD:
+        tile.set_tile_mins(mins[0:3])
+        tile.set_tile_maxs(maxs[0:3])
+    elif conf.twoD:
+        tile.set_tile_mins(mins[0:2])
+        tile.set_tile_maxs(maxs[0:2])
+    elif conf.oneD:
+        tile.set_tile_mins(mins[0:1])
+        tile.set_tile_maxs(maxs[0:1])
+
+    return
+
+
+# load virtual tiles
+def load_virtual_tiles(n, conf):
+
+    for cid in n.get_virtual_tiles():
+        tile_orig = n.get_tile(cid)
+        ind = tile_orig.index
+
+        # new prtcl tile;
+        # TODO: load_metainfo *HAS* to be after add_tile because
+        # add_tile modifies tile content.
+
+        if conf.threeD:
+            i,j,k = ind
+            tile = pypic.threeD.Tile(conf.NxMesh, conf.NyMesh, conf.NzMesh)
+
+            n.add_tile(tile, ind) 
+            tile.load_metainfo(tile_orig.communication)
+            initialize_tile(tile, (i,j,k), n, conf)
+
+        elif conf.twoD:
+            i,j = ind
+            tile = pypic.twoD.Tile(conf.NxMesh, conf.NyMesh, conf.NzMesh)
+
+            n.add_tile(tile, ind) 
+            tile.load_metainfo(tile_orig.communication)
+            initialize_tile(tile, (i,j,0), n, conf)
+        
+        elif conf.oneD:
+            i, = ind
+            tile = pypic.oneD.Tile(conf.NxMesh, conf.NyMesh, conf.NzMesh)
+
+            n.add_tile(tile, ind) 
+            tile.load_metainfo(tile_orig.communication)
+            initialize_tile(tile, (i,0,0), n, conf)
+
+    return 
+
+
+
+# 3D loading of pic tiles into grid
+def load_tiles(n, conf):
+
+    for k in range(n.get_Nz()):
+        for j in range(n.get_Ny()):
+            for i in range(n.get_Nx()):
+                # print("{} ({},{}) {} ?= {}".format(n.rank, i,j, n.get_mpi_grid(i,j), ref[j,i]))
+
+                if conf.threeD:
+                    if n.get_mpi_grid(i, j, k) == n.rank():
+                        tile = pypic.threeD.Tile(conf.NxMesh, conf.NyMesh, conf.NzMesh)
+
+                        ind = (i, j, k)
+                        initialize_tile(tile, (i,j,k), n, conf)
+                        n.add_tile(tile, ind)
+
+                elif conf.twoD:
+                    if n.get_mpi_grid(i, j) == n.rank():
+                        tile = pypic.twoD.Tile(conf.NxMesh, conf.NyMesh, conf.NzMesh)
+                        ind = (i, j)
+
+                        initialize_tile(tile, (i,j,k), n, conf)
+                        n.add_tile(tile, ind)
+                
+                elif conf.oneD:
+                    if n.get_mpi_grid(i) == n.rank():
+                        tile = pypic.oneD.Tile(conf.NxMesh, conf.NyMesh, conf.NzMesh)
+                        ind = (i,)
+
+                        initialize_tile(tile, (i,j,k), n, conf)
+                        n.add_tile(tile, ind)
+
+    return
