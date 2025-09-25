a0a0e239a8061f2aecf597236d6b1592371c3653
natj <nattila.joonas@gmail.com>
2025-09-25 16:14:32 +0300

    fix to imports from old next module

--- runko/__init__.py
+++ runko/__init__.py
@@ -1,9 +1,9 @@
-from pyrunko._runko_next import ParticleState
+from pyrunko.pic.threeD import ParticleState
 from .configuration import Configuration
 from .tile_grid import TileGrid
-import pyrunko.emf2.threeD as emf
-import pyrunko.pic2.threeD as pic
-from pyrunko.pic2.threeD import ParticleStateBatch
+import pyrunko.emf.threeD as emf
+import pyrunko.pic.threeD as pic
+from pyrunko.pic.threeD import ParticleStateBatch
 from .simulation import Simulation
 from pyrunko.tools import comm_mode, particle
 from .runko_logging import on_main_rank, runko_logger, runko_default_handler
