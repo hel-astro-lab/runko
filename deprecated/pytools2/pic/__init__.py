cdbbd8ddaf83f83995ac3b99885e3c61de036465
natj <nattila.joonas@gmail.com>
2025-09-27 11:36:11 +0300

    merge deprecated pytools and pytools dirs

--- deprecated/pytools2/pic/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*- 
-
-from .tile_initialization import initialize_tile
-from .tile_initialization import load_tiles
-from .tile_initialization import load_virtual_tiles
-
-from .injector import inject
