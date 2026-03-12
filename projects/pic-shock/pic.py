d47e24d6712101c92dcd1c1a3b2d63a6375475d9
natj <nattila.joonas@gmail.com>
2026-03-12 17:20:36 +0200

    fix(shock): update shock setup to the latest io capabilities

--- projects/pic-shock/pic.py
+++ projects/pic-shock/pic.py
@@ -227,6 +227,7 @@ if __name__ == "__main__":
 
         if simulation.lap % output_interval == 0:
             x.io_emf_snapshot()
+            x.io_prtcl_snapshot()
             simulation.log_timer_statistics()
 
     simulation.for_each_lap(pic_simulation_step)
