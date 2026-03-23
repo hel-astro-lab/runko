30cfa466d693ee96f4fb68e206b908c875e81483
Miro Palmu <email@miropalmu.cc>
2026-03-23 09:17:55 +0200

    fix(tools): static const -> static constexpr

--- tools/fastlog.h
+++ tools/fastlog.h
@@ -7,7 +7,7 @@
 constexpr int fastlog2(uint32_t v) {
   // http://graphics.stanford.edu/~seander/bithacks.html
   int r;
-  static const int MultiplyDeBruijnBitPosition[32] = 
+  static constexpr int MultiplyDeBruijnBitPosition[32] =
   {
     0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30,
     8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31
