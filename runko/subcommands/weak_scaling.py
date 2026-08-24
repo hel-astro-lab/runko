d61db8023f9bcb84d26476b4ee8bf32d58043551
Miro Palmu <email@miropalmu.cc>
2026-08-24 13:19:56 +0300

    feat(cli/weak-scaling): add --stackplot

--- runko/subcommands/weak_scaling.py
+++ runko/subcommands/weak_scaling.py
@@ -155,7 +155,14 @@ def main(argv: list[str]):
                         Maximum split is defined to be the difference between the slowest
                         and the fastest rank on the same lap.
                         All available laps are searched for the largest maximum split.
-                        Incompatible with: --draw-all, --shade-bounds, --plot-totals and --stackplot.
+                        Incompatible with: --draw-all, --shade-bounds and --plot-totals.
+                        """)
+
+    parser.add_argument("--stackplot",
+                        action="store_true",
+                        help="""
+                        Plots the slowest times accross ranks as a stackplot.
+                        Incompatible with: --draw-all, --shade-bounds and --plot-totals.
                         """)
 
     parser.add_argument("--draw-all",
@@ -184,11 +191,15 @@ def main(argv: list[str]):
     # Maps args.* to what argument --* it actually represents.
     name_map = lambda x: "--" + x.replace("_", "-")
 
-    arg_conflicts = {"max_splits": ["draw_all", "shade_bounds", "plot_totals"]}
+    arg_conflicts = {"max_splits": ["draw_all", "shade_bounds", "plot_totals"],
+                     "stackplot": ["draw_all", "shade_bounds", "plot_totals"]}
+
+    # This assumes that arg_confligs only contains action=store_true args.
+    arg_is_given = lambda x: hasattr(args, x) and getattr(args, x)
     for arg, conflicts in arg_conflicts.items():
-        if hasattr(args, arg):
+        if arg_is_given(arg):
             for x in conflicts:
-                if hasattr(args, x):
+                if arg_is_given(x):
                     print(f"error: {name_map(x)} does not make sense with {name_map(arg)}")
                     exit(1)
 
@@ -197,14 +208,25 @@ def main(argv: list[str]):
     # Load heavy modules only after argument parsing.
     import runko
 
-    fig, (ax_legend, ax) = plt.subplots(2, 1,
-                                        layout="constrained",
-                                        figsize=(8, 6),
-                                        dpi=args.dpi)
+    disable_legend = args.stackplot
 
+    if disable_legend:
+        fig, (ax) = plt.subplots(1, 1,
+                                 layout="constrained",
+                                 figsize=(1.6 * 6, 6),
+                                 dpi=args.dpi)
+    else:
+        fig, (ax_legend, ax) = plt.subplots(2, 1,
+                                            layout="constrained",
+                                            figsize=(8, 6),
+                                            dpi=args.dpi)
+
+    grid_alpha = 0.2
+    if args.stackplot:
+        grid_alpha = 0.4
     ax.grid(which="both",
             linewidth=1,
-            alpha=0.2)
+            alpha=grid_alpha)
 
     # (ranks, name) -> (n_particles, list[TimingStatistic])
     data = dict()
@@ -252,12 +274,14 @@ def main(argv: list[str]):
             if args.push_time:
                 max_split *= ranks / n_particles
 
-            ax.scatter([ranks],
-                       [max_split],
-                       label=name,
-                       color=c,
-                       marker=m)
-            # max_splits uses h to draw the lines that connect the markers.
+            if not args.stackplot:
+                ax.scatter([ranks],
+                           [max_split],
+                           label=name,
+                           color=c,
+                           marker=m)
+
+            # max_splits uses h.
             h = max_split
         else:
             for s in stats:
@@ -269,14 +293,14 @@ def main(argv: list[str]):
                 l = min(l, t)
                 h = max(h, t)
 
-                if args.draw_all:
+                if args.draw_all and not args.stackplot:
                     ax.scatter([ranks],
                                [t],
                                label=name,
                                color=c,
                                marker=m)
 
-            if not args.draw_all:
+            if not args.draw_all and not args.stackplot:
                 ax.scatter([ranks, ranks],
                            [l, h],
                            label=name,
@@ -328,40 +352,97 @@ The outdirs might not contain any timing data"""
             r, l, h = sorted_by_key(r, l, h)
             ax.fill_between(r, l, h, alpha=0.1, color=c)
 
-    if args.max_splits:
+    if args.max_splits and not args.stackplot:
         for name, (r, _, h) in lines.items():
             c, _ = string_to_color_n_marker(name)
             r, h = sorted_by_key(r, h)
             ax.plot(r, h, alpha=0.2, color=c)
 
+    if args.stackplot:
+        shared_r = None
+        names = []
+        data = []
+        colors = []
 
-    # Filter out duplicate entries from the legend.
-    handles, labels = ax.get_legend_handles_labels()
-    unique_labels_set = set()
-    unique_labels = []
-    unique_handles = []
+        for name, (r, l, h) in lines.items():
+            colors.append(string_to_color_n_marker(name)[0])
 
-    for h, l in zip(handles, labels):
-        if l not in unique_labels_set:
-            unique_labels_set.add(l)
-            unique_labels.append(l)
-            unique_handles.append(h)
+            r, h = sorted_by_key(r, h)
+            if shared_r and shared_r != r:
+                raise RuntimeError("Internal logic error. Please send bug report :)")
+            else:
+                shared_r = r
 
-    # Sort labels based on the name.
-    zipped = list(zip(unique_labels, unique_handles))
-    zipped.sort(key=lambda x: x[0])
-    unique_labels, unique_handles = zip(*zipped)
+            names.append(name)
+            data.append(h)
 
-    ax_legend.legend(unique_handles,
-                     unique_labels,
-                     mode="expand",
-                     loc="lower left",
-                     borderaxespad=0,
-                     ncol=3)
+        if not shared_r or len(shared_r) <= 1:
+            raise RuntimeError("stackplot error: there is not enough data to plot")
 
-    ax_legend.set(frame_on=False,
-                  xticks=[],
-                  yticks=[])
+        key = [-x[-1] for x in data]
+        _, data, colors, names = sorted_by_key(key, data, colors, names)
+        ax.stackplot(shared_r, data,
+                     colors=colors,
+                     edgecolor="black",
+                     alpha=0.6)
+
+        # Write labels manually:
+        height_rhs = sum([x[-1] for x in data])
+        next_anno_y = data[0][-1] * 0.5
+        anno_x = shared_r[-1]
+
+        for n, name in enumerate(names):
+
+            # if data[n][-1] / height_rhs < 0.05:
+
+            number_of_lines = 0
+            if not n < number_of_lines:
+                arrow = None
+            else:
+                total_arm = 20
+                armB = 2 + int(total_arm * n / number_of_lines)
+                armA = 2 + total_arm - armB
+                arrow = dict(arrowstyle="-",
+                             connectionstyle=f"arc,angleA=-180,angleB=0,armA={armA},armB={armB},rad=0",
+                             relpos=(0, 0.5),
+                             color=colors[n])
+
+            ax.annotate(name,
+                        xy=(anno_x, next_anno_y),
+                        xycoords="data",
+                        xytext=(1.02, n / len(names)),
+                        textcoords=ax.transAxes,
+                        color=colors[n],
+                        arrowprops=arrow)
+
+            if n + 1 < len(names):
+                next_anno_y += 0.5 * (data[n][-1] + data[n + 1][-1])
+
+    def setup_legend():
+        # Filter out duplicate entries from the legend.
+        handles, labels = ax.get_legend_handles_labels()
+        unique_labels_set = set()
+        unique_labels = []
+        unique_handles = []
+
+        for h, l in zip(handles, labels):
+            if l not in unique_labels_set:
+                unique_labels_set.add(l)
+                unique_labels.append(l)
+                unique_handles.append(h)
+
+        unique_labels, unique_handles = sorted_by_key(unique_labels, unique_handles)
+        ax_legend.legend(unique_handles,
+                         unique_labels,
+                         mode="expand",
+                         loc="lower left",
+                         borderaxespad=0,
+                         ncol=3)
+
+        ax_legend.set(frame_on=False, xticks=[], yticks=[])
+
+    if not disable_legend:
+        setup_legend()
 
     if args.max_splits:
         if args.push_time:
@@ -374,15 +455,22 @@ The outdirs might not contain any timing data"""
         else:
             ylabel = "average time [s]"
 
-    ax.set(xscale="log",
-           yscale="log",
-           xticks=list(seen_ranks),
-           xlabel="ranks",
-           ylabel=ylabel,
-           ylim=(args.ymin, None))
+    if args.stackplot:
+        ax.set(xscale="log",
+               yscale="linear",
+               xlabel="ranks",
+               ylabel=ylabel)
+    else:
+        ax.set(xscale="log",
+               yscale="log",
+               xlabel="ranks",
+               ylabel=ylabel,
+               ylim=(args.ymin, None))
 
-    # Required for xticks to work correctly with log scale.
+
+    ax.get_xaxis().set_major_locator(mpl.ticker.FixedLocator(list(seen_ranks)))
     ax.get_xaxis().set_major_formatter(mpl.ticker.ScalarFormatter())
+    ax.get_xaxis().set_minor_locator(mpl.ticker.NullLocator())
 
     if args.savefig:
         fig.savefig(args.savefig)
